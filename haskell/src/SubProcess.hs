f4b8e111af6f864021d2ec8c49c29522a756e03c
doyougnu <jmy6342@gmail.com>
2017-10-02 13:26:30 -0700

    minisat wrapper done

--- haskell/src/SubProcess.hs
+++ haskell/src/SubProcess.hs
@@ -1,14 +1,21 @@
 module SubProcess where
 
-import Turtle
+import qualified Turtle as T
 import Turtle.Line
 import Data.Text (pack)
 import System.Posix.Process (getProcessID)
 
 import CNF
 
-toLine :: CNF -> Shell Line
-toLine = select . textToLines . pack . show
+toLine :: CNF -> T.Shell Line
+toLine = T.select . textToLines . pack . show
 
-mkTmpFileName :: Shell Line
-mkTmpFileName = undefined
+tmpFileName :: T.FilePath
+tmpFileName = ".tmp"
+
+runMinisat :: T.FilePath -> CNF -> IO T.ExitCode
+runMinisat f cnf = do
+  T.output f (toLine cnf)
+  exit <- T.proc "minisat" [T.format T.fp f] T.empty
+  T.rm f
+  return exit
