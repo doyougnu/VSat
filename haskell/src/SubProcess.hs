7f0e0d03164df05b3156e01572044951d0fb273d
doyougnu <jmy6342@gmail.com>
2017-10-18 10:03:35 -0700

    adding comments and abstracting out sat solver

--- haskell/src/SubProcess.hs
+++ haskell/src/SubProcess.hs
@@ -7,12 +7,19 @@ import System.Posix.Process (getProcessID)
 
 import CNF
 
+-- | Take anything that can be shown and pack it into a shell line
 toLine :: (Show a) => a -> T.Shell Line
 toLine = T.select . textToLines . pack . show
 
--- runMinisat :: T.FilePath -> CNF -> IO Bool
-runMinisat :: CNF -> IO (T.Shell Line)
-runMinisat cnf = do
-  let output = T.inproc "minisat" [] (toLine cnf)
-      res = T.grep (T.has "restarts") output
-  return res
+-- | Take any Sat solver that can be called from shell, and a plain CNF term
+-- and run the CNF through the specified SAT solver
+run :: T.Text -> CNF Plain -> IO ()
+run sat cnf = do
+  let output = T.inproc sat [] (toLine cnf)
+      res = T.grep (T.has "SATISFIABLE") output
+  T.view res
+
+-- | Take any plain CNF term and run it through the SAT solver
+-- Run like: runMinisat $ toPlain [(3, True)] vEx1
+runMinisat :: CNF Plain -> IO ()
+runMinisat = run "minisat"
