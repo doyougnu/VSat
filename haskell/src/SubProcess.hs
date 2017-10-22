8bac6024b5b35415cd3c73428d907cd7d7cafff5
doyougnu <jmy6342@gmail.com>
2017-10-22 14:29:25 -0700

    Adding genConfig, and toPlain to right file, brute force almost done

--- haskell/src/SubProcess.hs
+++ haskell/src/SubProcess.hs
@@ -4,6 +4,7 @@ import qualified Turtle as T
 import Turtle.Line
 import Data.Text (pack)
 import System.Posix.Process (getProcessID)
+import Data.Maybe (catMaybes, fromJust)
 
 import CNF
 import TagTree
@@ -12,10 +13,25 @@ import TagTree
 toLine :: (Show a) => a -> T.Shell Line
 toLine = T.select . textToLines . pack . show
 
+-- | Given a Variational CNF generate a config for all choices
+genConfig :: CNF Variational V -> Config
+genConfig = concatMap (\x -> [(x, True), (x, False)]) . catMaybes . fmap tag
+            . concatMap (filter isChc) . clauses
+
+-- | Given a config and a Variational CNF, transform to a Plain CNF
+toPlain :: Config -> CNF Variational V -> CNF Plain V
+toPlain cs CNF{comment,vars,clauses} =
+  CNF { comment=comment
+      , vars=vars
+      , clauses = fmap (fmap $ one . fromJust . select cs) clauses
+      }
+
+
+
 -- | Take any Sat solver that can be called from shell, and a plain CNF term
 -- and run the CNF through the specified SAT solver
-run :: T.Text -> CNF Plain V -> IO ()
-run sat cnf = do
+runPlain :: T.Text -> CNF Plain V -> IO ()
+runPlain sat cnf = do
   let output = T.inproc sat [] (toLine cnf)
       res = T.grep (T.has "SATISFIABLE") output
   T.view res
@@ -23,4 +39,4 @@ run sat cnf = do
 -- | Take any plain CNF term and run it through the SAT solver
 -- Run like: runMinisat $ toPlain [(3, True)] vEx1
 runMinisat :: CNF Plain V -> IO ()
-runMinisat = run "minisat"
+runMinisat = runPlain "minisat"
