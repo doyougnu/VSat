5b38a0b80a3013601998bcecf5469c8fa9e2ca37
doyougnu <jmy6342@gmail.com>
2017-10-22 17:25:52 -0700

    Fixing redundancy bug in genConfigs

--- haskell/src/SubProcess.hs
+++ haskell/src/SubProcess.hs
@@ -5,13 +5,16 @@ import Turtle.Line
 import qualified Data.Text as D (pack)
 import Data.Maybe (fromJust)
 import qualified Control.Foldl as F
-import Data.List (groupBy)
+import Data.List (groupBy, nub)
 import Data.Function (on)
 
 import CNF
 import TagTree
 
 type Satisfiable = Bool
+
+-- | A result is a particular configuration, and its satisfiability result
+type Result = (Config, Satisfiable)
 
 -- | Take anything that can be shown and pack it into a shell line
 toLine :: (Show a) => a -> T.Shell Line
@@ -20,7 +23,7 @@ toLine = T.select . textToLines . D.pack . show
 -- | Given a Variational CNF generate a config for all choices
 genConfig :: CNF Variational V -> [Config]
 genConfig cnf = sequence $ groupBy ((==) `on` fst) configs
-  where tags' = concatMap tags . concat . filter (any isChc) $ clauses cnf
+  where tags' = nub . concatMap tags . concat . filter (any isChc) $ clauses cnf
         configs = (,) <$> tags' <*> [True, False]
 
 -- | Given a config and a Variational CNF, transform to a Plain CNF
@@ -45,7 +48,7 @@ run sat cnf = do
 
 -- | take any Sat solver that can be called from shell, and any variational CNF
 -- term, and run all combinations of the CNF through the SAT solver
-runV :: T.Text -> CNF Variational V -> IO [(Config, Satisfiable)]
+runV :: T.Text -> CNF Variational V -> IO [Result]
 runV solver cnf = do
   results <- sequence $ run solver <$> plains
   let returnVals = zip configs results
@@ -63,5 +66,9 @@ runPMinisat :: CNF Plain V -> IO Satisfiable
 runPMinisat = run "minisat"
 
 -- | Take any variational CNF term and run it through the SAT solver
-runVMinisat :: CNF Variational V -> IO [(Config, Satisfiable)]
+runVMinisat :: CNF Variational V -> IO [Result]
 runVMinisat = runV "minisat"
+
+-- | Given a list of results, only return the failures
+failures :: [Result] -> [Result]
+failures = filter ((==False) . snd)
