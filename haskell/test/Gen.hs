0da650cf6a12aa28c97f629fe318550f9dd1f753
doyougnu <jmy6342@gmail.com>
2018-01-15 16:54:53 -0800

    better string generation

--- haskell/test/Gen.hs
+++ haskell/test/Gen.hs
@@ -7,6 +7,20 @@ import VProp
 genDim :: Gen String
 genDim = arbitrary :: Gen String
 
+genAlphaNum :: Gen Char
+genAlphaNum = elements ['a'..'z']
+
+genAlphaNumStr :: Gen String
+genAlphaNumStr = listOf genAlphaNum
+
+newtype ANString = ANString {unwrapASString :: String}
+
+instance Show ANString where
+  show = show . unwrapASString
+
+instance Arbitrary ANString where
+  arbitrary = ANString <$> genAlphaNumStr
+
 instance (Arbitrary d, Arbitrary a) => Arbitrary (VProp d a) where
   arbitrary = sized arbVProp
 
