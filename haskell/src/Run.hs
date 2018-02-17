bc3fa1765bfd74698e6cee5611ef92af8c4ec61e
doyougnu <jmy6342@gmail.com>
2018-02-17 11:59:20 -0800

    sticking with integer need to debug work' function

--- haskell/src/Run.hs
+++ haskell/src/Run.hs
@@ -7,9 +7,8 @@ import Data.Bifunctor (bimap)
 import Data.List (nub)
 import Data.Bifoldable
 import Data.Foldable (foldr')
--- import Data.Maybe (fromJust, isJust)
+import Data.Maybe (fromJust, isJust)
 import qualified Data.Map.Strict as M
--- import qualified Data.IntMap.Strict as I
 import qualified Data.Set as S (fromList)
 import Control.Monad.RWS.Strict
 
@@ -19,10 +18,10 @@ import SubProcess
 
 -- | A variable dictionary mapping with dimensions or integers to integers,
 -- required for the sat solver
-type VarDict d a = M.Map (Either d a) Int
+type VarDict d a = M.Map (Either d a) Integer
 
 -- | a intmap that maps integers back to the dimension or value names
-type VarDictR d a = M.Map Int (Either d a)
+type VarDictR d a = M.Map Integer (Either d a)
 
 -- | The satisfiable dictionary, this is actually the "state" keys are configs
 -- and values are whether that config is satisfiable or not (a bool)
@@ -43,7 +42,7 @@ type Env d a r = RWST (Opts d a) Log (SatDict d) IO r -- ^ the monad stack
 
 -- | An empty reader monad environment, in the future read these from config file
 _emptyOpts :: Opts d a
-_emptyOpts = Opts { baseline = True -- set to use andDecomp
+_emptyOpts = Opts { baseline = False -- set to use andDecomp
                  , optimizations = []
                  , vars = M.empty
                  , rvars = M.empty
@@ -53,7 +52,7 @@ _emptyOpts = Opts { baseline = True -- set to use andDecomp
 runEnv :: Env d a r -> Opts d a -> SatDict d -> IO (r, SatDict d,  Log)
 runEnv m opts st = runRWST m opts st
 
-run :: (Show d, Show a, Ord a, Ord d) =>
+run :: (Show d, Show a, Ord a, Ord d, Integral a) =>
   VProp d a -> IO (Maybe (VProp d Satisfiable), SatDict d, Log)
 run x = runEnv (work x) (_recordVars x _emptyOpts) (initSt x)
 
@@ -78,19 +77,19 @@ collect = nub . bifoldr'
 
 
 -- | Given a list of dimensions and values and an integer construct the vardict
-genVDict :: (Ord a, Ord d) => [(Either d a, Int)] -> VarDict d a
+genVDict :: (Ord a, Ord d) => [(Either d a, Integer)] -> VarDict d a
 genVDict = M.fromList
 
 
 -- | Given a list of dimensions and values and an integer construct the reverse
 -- vardict
-genRVDict :: [(Either d a, Int)] -> VarDictR d a
+genRVDict :: [(Either d a, Integer)] -> VarDictR d a
 genRVDict = foldr' (\(dim, int) dict -> M.insert int dim dict) M.empty
 
 
 -- | Given a variable dictionary and a vprop term. Construct a representative
 -- VProp with only integers for both dimensions and variables
-unify :: (Ord d, Ord a) => VarDict d a -> VProp d a ->  VProp Int Int
+unify :: (Ord d, Ord a) => VarDict d a -> VProp d a ->  VProp Integer Integer
 unify vDict = bimap ((vDict M.!) . Left) ((vDict M.!) . Right)
 
 
@@ -127,29 +126,32 @@ initSt vs = M.fromList $ zip (paths vs) (repeat False)
 
 
 -- | convert  propositional term to a DIMACS CNF term
-propToCNF :: (Num a, Integral a) => String -> GProp a -> C.CNF
+propToCNF :: String -> GProp Integer -> C.CNF
 propToCNF str ps = cnf
   where
     cnf = C.CNF { C.comment = str
-                , C.vars    = S.fromList $ foldr' ((:) . toInteger) [] ps
-                , C.clauses = orSplit . toListAndSplit $ toInteger <$> ps
+                , C.vars    = S.fromList $ foldr' (:) [] ps
+                , C.clauses = orSplit . toListAndSplit $ ps
                 }
 
 -- | given a variable dictionary and a vprop, replace all dimenions with the
 -- values in the dict
-packProp :: (Ord a, Ord d) => VProp d a -> VarDict d a -> VProp Int Int
+packProp :: (Ord a, Ord d) => VProp d a -> VarDict d a -> VProp Integer Integer
 packProp ps dict = bimap ((dict M.!) . Left) ((dict M.!) . Right) ps
 
 
--- | Given a reverse variable dictionary and a VProp Int a replace all
+-- | Given a reverse variable dictionary and a VProp Integer a replace all
 -- dimensions with their values in the reverse variable dictionary
-unPackProp :: (Ord a, Ord d) => VProp Int Int -> VarDictR d a -> VProp d a
+unPackProp :: (Ord a, Ord d) => VProp Integer Integer -> VarDictR d a -> VProp d a
 unPackProp ps dict = bimap (getL . (dict M.!)) (getR . (dict M.!)) ps
 
 
 -- | main workhorse for running the SAT solver
--- FIXE THE ENGINE CALL SO YOU CAN RUN SOMETHING
--- run like: runEnv (work _ex) (_recordVars _ex _emptyOpts)
+
+work :: (Show a, Show d, Ord a, Ord d, MonadReader (Opts d a) (t IO),
+          MonadTrans t,
+          MonadState (SatDict d) (t IO)) =>
+        VProp d a -> t IO (Maybe (VProp d Satisfiable))
 work cs = do
   bs <- asks baseline
   vdict <- asks vars
@@ -163,32 +165,34 @@ work cs = do
     return aa
 
     else do
-    -- let keys = M.keys sats
-    -- cnfs = (\y -> (y, select y cs)) <$> keys
-    -- mapM_ work' cnfs
-    -- (_, newSats, rvars) <- get
+    let confs = M.keys sats
+        cnfs = (\y ->
+                  (y,  fmap (flip packProp vdict) . flip select cs $ y)) <$> confs
+    mapM_ work' cnfs
     -- let x = recompile (M.toList newSats)
-    -- y = bimap (yankOut rvars) id  <$> y
+    --     y = bimap (yankOut rvars) id  <$> y
     -- return $ recompile (M.toList (M.map (const res) sats))
     return aa
 
 
--- -- | Given a configuration, a boolean representing satisfiability and a Prop, If
--- -- the prop does not contain a Nothing (as denoted by the bool) then extract the
--- -- values from the prop, ground then prop, convert to a CNF with descriptor of
--- -- the configuration, run the sat solver and save the result to the SAT table
--- -- TODO: Fix this nonsensical type signature
--- work' (conf, prop) = when (isJust prop) $
---   do (vars, sats, rvars) <- get
---      result <- lift . runPMinisat . propToCNF (show conf) . fmap fromJust . ground conf . fromJust $ prop
---      put (vars, M.insert conf result sats, rvars)
+-- | Given a configuration, a boolean representing satisfiability and a Prop, If
+-- the prop does not contain a Nothing (as denoted by the bool) then extract the
+-- values from the prop, ground then prop, convert to a CNF with descriptor of
+-- the configuration, run the sat solver and save the result to the SAT table
+work' :: (Ord d, Show d, MonadTrans t, MonadState (SatDict d) (t IO)) =>
+         (M.Map d Bool, Maybe (VProp Integer Integer)) -> t IO ()
+work' (conf, prop) = when (isJust prop) $
+  do
+    sats <- get
+    result <- lift . runPMinisat . propToCNF (show conf) . fmap fromJust . ground conf . fromJust $ prop
+    put (M.insert conf result sats)
 
 
-_ex :: VProp String Int
+_ex :: VProp String Integer
 _ex = Chc "a" (Chc "b" (Ref 1) (Ref 2)) (Chc "c" (Ref 1) (Ref 2))
 
-_ex1 :: VProp String Int
+_ex1 :: VProp String Integer
 _ex1 = Chc "a" (Ref 1) (Ref 2)
 
-_ex2 :: VProp Int Int
+_ex2 :: VProp Integer Integer
 _ex2 = Chc 0 (Chc 1 (Ref 1) (Ref 2)) (Chc 2 (Ref 1) (Ref 2))
