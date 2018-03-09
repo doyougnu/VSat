3f363bec01feb8f4e633040ec13e5e95b2749a09
doyougnu <jmy6342@gmail.com>
2018-03-09 10:10:17 -0800

    minor fixes

--- haskell/bench/Baselines.hs
+++ haskell/bench/Baselines.hs
@@ -1,20 +1,16 @@
 import Criterion.Main as C
 
 import Run
-import VProp (numTerms,numChc,depth,genVProp,VProp)
+import VProp (genVProp)
 
-
-runner :: Bool -> VProp -> Benchmark
-runner bool x = bench ("\nNumTerms: " ++ (show $ numTerms x) ++ "\n" ++
-                        "NumChc: " ++ (show $ numChc x) ++ "\n" ++
-                        "Depth: " ++ (show $ depth x)) $ C.whnfIO (runEnv bool x)
 
 main :: IO ()
 main = do
-        props <- sequence . take 5 $ repeat genVProp
+        prop <- genVProp
+        print prop
         C.defaultMain
           [ C.bgroup "Baselines"
-            [ bench "Brute Force" $ C.whnfIO (runEnv False (head props))
-            , bench "And Decomposition" $ C.whnfIO (runEnv True (head props))
+            [ bench "Brute Force" $ C.whnfIO (runEnv False prop)
+            , bench "And Decomposition" $ C.whnfIO (runEnv True prop)
             ]
           ]
