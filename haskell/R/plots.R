61d6ca3c26d50455d20a49f67cf251b6cb7c1710
doyougnu <jmy6342@gmail.com>
2018-04-29 18:03:27 -0700

    Drying out code

--- haskell/R/plots.R
+++ haskell/R/plots.R
@@ -14,55 +14,77 @@ library(ggmosaic)
                                         # read in each data table
 
 ## variables for each file, this is hard coded to correspond to criterion
-## output, check app/main.hs
+## output, check app/main.hs the timing results are the actual measurements
+## taken by criterion and the recorded bgroup names
 timingsResultsFile <- "../results.csv"
+
+## the descriptor results are the hand crafted descriptor functions for each
+## measurement that are recorded to a csv via cassava, these are things like
+## number of choices in the prop, number of terms etc.
 descriptorResults <- "../timings.csv"
 
-## read in timings table
-dfTimings <- read.csv(file=timingsResultsFile)
+## Given a dataframe that assumes the output structure of criterion's --csv call
+## clean up the data frame by converting numerics to numerics while maintaining
+## the names columns and tidy up the data set
+cleanTimings <- function(df_) {
 
-## grab the naming column
-nmCol <- dfTimings$Name
+  ## grab the naming column
+  nmCol <- df_$Name
 
-## mutate the numerics from characters to numerics
-timings <- dfTimings[,2:7] %>% apply(2, as.numeric) %>% as.data.frame
+  ## mutate the numerics from characters to numerics
+  df_ <- df_[,2:7] %>% apply(2, as.numeric) %>% as.data.frame
 
-## Reconstruct the data frame
-timings$Name <- nmCol
-timings <- timings %>% filter(!is.na(Mean))
+  ## Reconstruct the data frame
+  df_$Name <- nmCol
+  df_ <- df_ %>% filter(!is.na(Mean))
 
-## tidy up the data, each column is a variable and each row is an observation
-timings <- timings %>%
-  separate(Name, into = c("shared", "scale", "Operation"), sep = "\\/") %>%
-  mutate(scale = as.numeric(scale))
+  ## tidy up the data, each column is a variable and each row is an observation
+  df_ <- df_ %>%
+    separate(Name, into = c("shared", "scale", "Operation"), sep = "\\/") %>%
+    mutate(scale = as.numeric(scale))
+
+  ## return
+  df_
+}
+
+cleanDesc <- function(df_) {
+
+  ## Clean up the trailing "_" in the column names
+  names(df_) <- gsub(pattern = "_", "", x = names(df_))
+
+  ## criterion adds a header for every test, this means we must drop every even
+  ## row because the first header is counted as a head in fread, which means our
+  ## data begins on row 1
+
+  ## define a sequence of evens (the rows we want to drop)
+  toDelete <- seq(0, length(df_), 2)
+
+  ## subset the table using the sequence and convert to numeric
+  df_ <- df_[-toDelete,,drop=F]
+
+  ## save the shared column
+  shared <- df_$shared
+
+  ## coerce the numbers to numbers
+  df_ <- df_[,2:7] %>% apply(2, as.numeric) %>% as.data.frame
+
+  ## add it back to the data frame
+  df_$shared <- shared
+
+}
+
+## helper function, given a file name, read in the data table and then apply a
+## unary function that works on tables to it
+readAndClean <- function(fname, f) {read.csv(file=fname) %>% f}
+
+## Read in the Timings table
+timings <- readAndClean(timingsResultsFile, cleanTimings)
 
 ## Read in descriptor table
-dfDesc <- read.csv(file=descriptorResults)
-
-## Clean up the trailing "_" in the column names
-names(dfDesc) <- gsub(pattern = "_", "", x = names(dfDesc))
-
-## criterion adds a header for every test, this means we must drop every even
-## row because the first header is counted as a head in fread, which means our
-## data begins on row 1
-
-## define a sequence of evens (the rows we want to drop)
-toDelete <- seq(0, length(dfDesc), 2)
-
-## subset the table using the sequence and convert to numeric
-dfDesc <- dfDesc[-toDelete,,drop=F]
-
-## save the shared column
-shared <- dfDesc$shared
-
-## coerce the numbers to numbers
-dfDesc <- dfDesc[,2:7] %>% apply(2, as.numeric) %>% as.data.frame
-
-## add it back to the data frame
-dfDesc$shared <- shared
+descriptors <- readAndClean(descriptorResults, cleanDesc)
 
 ## now merge the tables to a data frame
-df <- merge(timings, dfDesc)
+df <- merge(timings, descriptors)
 
 ############################## Plotting ########################################
 plot <- ggplot(df, aes(x=scale, y=Mean, color=Operation)) +
