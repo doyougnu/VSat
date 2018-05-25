d67c47221fbbb31f24350d95bf561875f419c7f8
doyougnu <jmy6342@gmail.com>
2018-05-24 17:14:25 -0700

    bug fixes, automated time stamps, uniq, shared data, all opts

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -46,9 +46,11 @@ eraseFile = flip writeFile ""
 -- run with stack build; stack bench --benchmark-arguments "timing_file desc_file"
 main :: IO ()
 main = do
-  (timingFile:descFile:_) <- getArgs >>= traverse (prependDate . (flip (++) ".csv"))
+  (folder:timingFile_:descFile_:_) <- getArgs
+  timingFile <- format timingFile_ folder
+  descFile   <- format descFile_ folder
   mapM_ eraseFile [descFile, timingFile]
-  mapM_ (benchRandomSample descFile timingFile) $ zip [1..] $ [10,20..500] >>= replicate 100
+  mapM_ (benchRandomSample descFile timingFile) $ zip [1..] $ [10,20..500] >>= replicate 200
 
 -- | The run number, used to join descriptor and timing data later
 type RunNum = Int
@@ -93,6 +95,8 @@ prependDate :: FilePath -> IO FilePath
 prependDate str = do (year, month, day) <- getCurrentTime >>= return . toGregorian . utctDay
                      return $ mconcat [show year, "-", show month, "-", show day, "-", str]
 
+format :: FilePath -> FilePath -> IO FilePath
+format fp fldr = prependDate (fp ++ ".csv") >>= return . ((++) (fldr ++ "/"))
 
 -- | Given some string, a run metric (the parameters for the run) a time and a
 -- file path, perform the IO effect
@@ -108,25 +112,25 @@ benchRandomSample :: FilePath -> FilePath -> RunMetric -> IO ()
 benchRandomSample descfp timefp metrics@(_, n) = do
   prop' <- generate (sequence $ repeat $ choose (0, 10)) >>=
           generate . genVPropAtSize n .  vPropShare
-  -- noShprop' <- generate (sequence $ repeat $ choose (0, 10)) >>=
-  --              generate . genVPropAtSize n .  vPropNoShare
+  noShprop' <- generate (sequence $ repeat $ choose (0, 10)) >>=
+               generate . genVPropAtSize n .  vPropNoShare
   let prop = fmap show prop'
-      -- noShprop = fmap show noShprop'
+      noShprop = fmap show noShprop'
 
   writeDesc "Shared" metrics prop descfp
-  -- writeDesc "Unique" metrics noShprop descfp
+  writeDesc "Unique" metrics noShprop descfp
 
   -- | run brute force solve
   time "Shared/BForce" metrics timefp $! runEnv True False False [] prop
-  -- time "Unique/BForce" metrics timefp $! runEnv True False False [] noShprop
+  time "Unique/BForce" metrics timefp $! runEnv True False False [] noShprop
 
   -- | run incremental solve
   time "Shared/VSolve" metrics timefp $! runEnv False False False [] prop
-  -- time "Unique/VSolve" metrics timefp $! runEnv False False False [] noShprop
+  time "Unique/VSolve" metrics timefp $! runEnv False False False [] noShprop
 
   -- | run and decomp
   time "Shared/ChcDecomp" metrics timefp $! runEnv True True False [] prop
-  -- time "Unique/ChcDecomp" metrics timefp $! runEnv True True False [] noShprop
+  time "Unique/ChcDecomp" metrics timefp $! runEnv True True False [] noShprop
 
 time :: NFData a => Text -> RunMetric -> FilePath -> IO a -> IO ()
 time !desc !metrics@(rn, n) timefp !a = do
