2f087f0588c43fdc57e3af7a5227630a90b2f22f
doyougnu <jmy6342@gmail.com>
2018-07-19 16:20:20 -0700

    minor refactor for better cohesion

--- haskell/src/Api.hs
+++ haskell/src/Api.hs
@@ -1,6 +1,6 @@
 module Api where
 
-import Data.SBV (SatResult(..), ThmResult(..), SMTResult)
+import Data.SBV (SatResult(..), ThmResult(..))
 
 import VProp.Types
 import Run
@@ -11,17 +11,9 @@ sat :: [VProp String String -> VProp String String]
   -> IO [V Dim (Maybe SatResult)]
 sat os p = fmap (bimap id (fmap SatResult)) . unbox . fst' <$> runVSMT os p
   where fst' (a,_,_) = a
-        unbox :: Result -> [V Dim (Maybe SMTResult)]
-        unbox (L _) = []
-        unbox (R _) = []
-        unbox (V xs) = xs
 
 prove :: [VProp String String -> VProp String String]
   -> VProp String String
   -> IO [V Dim (Maybe ThmResult)]
 prove os p = fmap (bimap id (fmap ThmResult)) . unbox . fst' <$> runVSMT os p
   where fst' (a,_,_) = a
-        unbox :: Result -> [V Dim (Maybe SMTResult)]
-        unbox (L _) = []
-        unbox (R _) = []
-        unbox (V xs) = xs
