d7d6e0165b0eff0fc8705f3cc5482c38fc9746fe
doyougnu <jmy6342@gmail.com>
2018-07-24 12:54:12 -0700

    cleaning up the same Dim types

--- haskell/src/Api.hs
+++ haskell/src/Api.hs
@@ -9,12 +9,10 @@ import Utils (fst')
 
 sat :: [VProp String String -> VProp String String]
   -> VProp String String
-  -> IO [V Dim (Maybe SatResult)]
+  -> IO [V String (Maybe SatResult)]
 sat os p = fmap (bimap id (fmap SatResult)) . unbox . fst' <$> runVSMT os p
-  where fst' (a,_,_) = a
 
 prove :: [VProp String String -> VProp String String]
   -> VProp String String
-  -> IO [V Dim (Maybe ThmResult)]
+  -> IO [V String (Maybe ThmResult)]
 prove os p = fmap (bimap id (fmap ThmResult)) . unbox . fst' <$> runVSMT os p
-  where fst' (a,_,_) = a
