edb112b1edcce8c81f55121bfe1e55461c663d63
doyougnu <jmy6342@gmail.com>
2018-07-24 13:51:06 -0700

    even better json serializer. Some thought it was true, but it is

--- haskell/src/Json.hs
+++ haskell/src/Json.hs
@@ -1,42 +1,11 @@
 module Json ( module Data.Aeson) where
 
-import V
-import Run (Result)
+import V (V(..))
 import Data.SBV (SatResult(..), SMTResult(..),ThmResult(..))
-import Data.SBV.Internals (SMTModel, showModel)
+import Data.SBV.Internals (showModel)
 
 import Data.Text
 import Data.Aeson
-
-
--- [Plain (Just Satisfiable. Model:
---   uvtkzmyevlwnjgnlrpqbhuwqolrgzs = False :: Bool
---   gdbulfwfqyhunejbhxvlfsrkfidzkx = False :: Bool
---   egqnikeuyt                     = False :: Bool
---   yxttobxccxhlkdypxwskzdm        = False :: Bool
---   ozojlitgquueborcdxafjpxoscutsn = False :: Bool
---   sedfvolsdlrhrysekdsjbs         = False :: Bool)
--- ,VChc (Dim {dimName = "BB"}) (Plain (Just Satisfiable. Model:
---   uvtkzmyevlwnjgnlrpqbhuwqolrgzs = False :: Bool
---   gdbulfwfqyhunejbhxvlfsrkfidzkx = False :: Bool
---   egqnikeuyt                     = False :: Bool
---   yxttobxccxhlkdypxwskzdm        = False :: Bool
---   ozojlitgquueborcdxafjpxoscutsn = False :: Bool
---   sedfvolsdlrhrysekdsjbs         = False :: Bool)) (Plain Nothing)
--- ,VChc (Dim {dimName = "AA"}) (Plain (Just Satisfiable. Model:
---   uvtkzmyevlwnjgnlrpqbhuwqolrgzs = False :: Bool
---   gdbulfwfqyhunejbhxvlfsrkfidzkx = False :: Bool
---   egqnikeuyt                     = False :: Bool
---   yxttobxccxhlkdypxwskzdm        = False :: Bool
---   ozojlitgquueborcdxafjpxoscutsn = False :: Bool
---   sedfvolsdlrhrysekdsjbs         = False :: Bool))
--- (Plain (Just Satisfiable. Model:
---   uvtkzmyevlwnjgnlrpqbhuwqolrgzs = False :: Bool
---   gdbulfwfqyhunejbhxvlfsrkfidzkx = False :: Bool
---   egqnikeuyt                     = False :: Bool
---   yxttobxccxhlkdypxwskzdm        = False :: Bool
---   ozojlitgquueborcdxafjpxoscutsn = False :: Bool
---   sedfvolsdlrhrysekdsjbs         = False :: Bool))]
 
 instance ToJSON SMTResult where
   toJSON (Unsatisfiable _) = object [("isSat" :: Text) .= ("Unsatisfiable" :: Text)]
@@ -50,4 +19,11 @@ instance ToJSON SMTResult where
 
 instance ToJSON SatResult where toJSON (SatResult x) = toJSON x
 instance ToJSON ThmResult where toJSON (ThmResult x) = toJSON x
--- instance ToJSON Run.Result
+
+instance (Show d, Show a, ToJSON a, ToJSON d) => ToJSON (V a d) where
+  toJSON (Plain x) = toJSON x
+  toJSON (VChc d l r) = object [ (pack (show d) :: Text) .=
+                                 object [ ("true" :: Text) .= toJSON l
+                                        , ("false" :: Text) .= toJSON r
+                                        ]
+                               ]
