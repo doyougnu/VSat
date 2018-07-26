a0ca15263ba33620d524f9f9e00c133e68284093
doyougnu <jmy6342@gmail.com>
2018-07-26 12:16:22 -0700

    adding monadic quickcheck tests

--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -1,6 +1,11 @@
 import Test.Tasty
+import Test.Tasty.QuickCheck
 
 import VProp.Core.Test
+import Run.Test
 
 main :: IO ()
-main = defaultMain coreProperties
+main = do defaultMain runProperties
+  where tests = testGroup "All" [ runProperties
+                                , coreProperties
+                                ]
