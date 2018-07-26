4efb069712bb1a6ab9795906844892ed48aab540
doyougnu <jmy6342@gmail.com>
2018-07-26 12:16:22 -0700

    adding monadic quickcheck tests

--- haskell/test/Run/Test.hs
+++ haskell/test/Run/Test.hs
@@ -3,6 +3,14 @@ module Run.Test where
 import Test.Tasty
 import Test.Tasty.QuickCheck as QC
 import Test.QuickCheck.Monadic as QCM
+import Data.SBV ( SatResult(..)
+                , SMTResult(..)
+                , ThmResult(..)
+                , SMTConfig(..)
+                , SMTSolver(..)
+                , Solver(..))
+import Data.SBV.Internals (showModel, SMTModel(..))
+
 
 import VProp.Types
 import VProp.Core
@@ -10,16 +18,63 @@ import VProp.SBV
 import VProp.Gen
 import Run
 
+instance Eq SMTResult where
+  (Unsatisfiable x) == (Unsatisfiable y) = x == y
+  (Satisfiable _ x) == (Satisfiable _ y) = x == y
+  (SatExtField _ x) == (SatExtField _ y) = x == y
+  (Unknown _ x)     == (Unknown _ y)     = x == y
+  (ProofError _ x)  == (ProofError _ y)  = x == y
+  _                 == _                 = False
+
+instance Eq Solver where
+  Z3        == Z3        = True
+  Yices     == Yices     = True
+  Boolector == Boolector = True
+  CVC4      == CVC4      = True
+  MathSAT   == MathSAT   = True
+  ABC       == ABC       = True
+  _         == _         = False
+
+instance Eq SMTModel where
+  m == n = (modelAssocs m) == (modelAssocs n)
+
+instance Eq SMTConfig where
+  (SMTConfig
+   {verbose             = a
+   ,timing              = _
+   ,printBase           = c
+   ,printRealPrec       = d
+   ,satCmd              = e
+   ,allSatMaxModelCount = f
+   ,isNonModelVar       = _
+   ,transcript          = h
+   ,smtLibVersion       = _
+   ,solver              = j
+   ,roundingMode        = _
+   ,solverSetOptions    = _
+   ,ignoreExitCode      = _
+   ,redirectVerbose     = n
+   }) == (SMTConfig{..}) = a == verbose &&
+                           c == printBase &&
+                           d == printRealPrec &&
+                           e == satCmd &&
+                           (==) f allSatMaxModelCount &&
+                           (==) h transcript &&
+                           (name j) == (name solver) &&
+                           (==) n redirectVerbose
+
+instance Eq SatResult where
+  (SatResult x) == (SatResult y) = x == y
+
 runProperties :: TestTree
 runProperties = testGroup "Run Properties" [qcProps]
 
-qcProps = testGroup "QuickChecked Properties" []
+qcProps = QC.testProperty "and decomp is correct" $ \x -> andDecomp_correct x
 
-
--- andDecomp_correct x = QCM.monadicIO $
---   do a <- QCM.run $ runAD [] (x :: VProp String String)
---      b <- QCM.run $ runAD [] x
---      assert ((head a) == (head b))
+andDecomp_correct x = QCM.monadicIO $
+  do a <- QCM.run $ runAD [] (x :: VProp String String)
+     b <- QCM.run $ runAD [] x
+     assert ((head a) == (head b))
 
 -- andDecomp_plain x = QCM.monadicIO $
 --   do a <- QCM.run $ runAD [] (x :: VProp String String)
