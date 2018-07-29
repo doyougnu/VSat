c14efe9d4613034e65edf0ff29718700fba8574c
doyougnu <jmy6342@gmail.com>
2018-07-28 17:45:48 -0700

    close to closing andDecomp explosion bug

--- haskell/src/VProp/SBV.hs
+++ haskell/src/VProp/SBV.hs
@@ -1,34 +1,34 @@
-module VProp.SBV ( module Data.SBV
-                 , andDecomp
+module VProp.SBV ( andDecomp
                  -- , shrinkPropExpr
                  , evalPropExpr
                  , symbolicPropExpr) where
 
-import           Data.SBV            ( (&&&)
-                                     , (|||)
-                                     , (==>)
-                                     , (<=>)
-                                     , Boolean
-                                     , Mergeable
-                                     , Predicate
-                                     , Uninterpreted
-                                     , EqSymbolic
-                                     , OrdSymbolic
-                                     , SBool
-                                     , SInteger
-                                     , SDouble
-                                     , true
-                                     , false
-                                     , bnot
-                                     , symbolicMerge
-                                     , unliteral
-                                     , literal
-                                     , sBools
-                                     , sInteger
-                                     , sIntegers
-                                     , uninterpret
-                                     , ite
-                                     , fromBool)
+import qualified Data.SBV as S
+-- ( (&&&)
+--                                      , (|||)
+--                                      , (==>)
+--                                      , (<=>)
+--                                      , Boolean
+--                                      , Mergeable
+--                                      , S.Predicate
+--                                      , S.Uninterpreted
+--                                      , S.EqSymbolic
+--                                      , S.OrdSymbolic
+--                                      , S.SBool
+--                                      , S.SInteger
+--                                      , S.SDouble
+--                                      , S.true
+--                                      , S.false
+--                                      , S.bnot
+--                                      , S.symbolicMerge
+--                                      , S.unliteral
+--                                      , S.literal
+--                                      , S.sBools
+--                                      , S.sInteger
+--                                      , S.sDouble
+--                                      , S.uninterpret
+--                                      , S.ite
+--                                      , S.fromBool)
 import           Prelude    hiding   (lookup,LT,EQ,GT)
 import           Data.Maybe          (fromMaybe)
 import           Data.Map            (fromList, lookup)
@@ -45,13 +45,13 @@ instance (Show a, Ord a) => SAT (VProp a a) where
 -- TODO fix this repetition
 -- | Evaluate a feature expression against a configuration.
 evalPropExpr :: DimBool
-             -> VConfig a SInteger
-             -> VConfig a SBool
+             -> VConfig a SNum
+             -> VConfig a S.SBool
              -> VProp a a
-             -> SBool
-evalPropExpr _ _ _ (LitB b)    = literal b
+             -> S.SBool
+evalPropExpr _ _ _ (LitB b)    = S.literal b
 evalPropExpr _ _ !c (RefB f)   = c f
-evalPropExpr d !i !c !(OpB Not e)   = bnot (evalPropExpr d i c e)
+evalPropExpr d !i !c !(OpB Not e)   = S.bnot (evalPropExpr d i c e)
 evalPropExpr d !i !c !(Opn And ps)  = foldr1 (&&&) $ evalPropExpr d i c <$> ps
 evalPropExpr d !i !c !(Opn Or ps)   = foldr1 (|||) $ evalPropExpr d i c <$> ps
 evalPropExpr d !i !c !(OpBB Impl l r)   = evalPropExpr d i c l ==> evalPropExpr d i c r
@@ -65,14 +65,14 @@ evalPropExpr d !i _  !(OpIB op l r)  = (handler op) (evalPropExpr' d i l) (evalP
         handler EQ  = (.==)
         handler NEQ = (./=)
 evalPropExpr d !i !c !(ChcB dim l r)
-  = ite (d dim) (evalPropExpr d i c l) (evalPropExpr d i c r)
+  = S.ite (d dim) (evalPropExpr d i c l) (evalPropExpr d i c r)
 
 -- | Eval the numeric expressions, VIExpr, assume everything is an integer until
 -- absolutely necessary to coerce
-evalPropExpr' :: DimBool -> VConfig a SInteger -> VIExpr a -> SNum
-evalPropExpr' _  _ !(LitI (I i)) = SI $ literal i
-evalPropExpr' _  _ !(LitI (D d)) = SD $ literal d
-evalPropExpr' _ !i !(Ref _ f)    = SI $ i f
+evalPropExpr' :: DimBool -> VConfig a SNum -> VIExpr a -> SNum
+evalPropExpr' _  _ !(LitI (I i)) = SI $ S.literal i
+evalPropExpr' _  _ !(LitI (D d)) = SD $ S.literal d
+evalPropExpr' _ !i !(Ref _ f)    = i f
 evalPropExpr' d !i !(OpI Neg e) = negate $ evalPropExpr' d i e
 evalPropExpr' d !i !(OpI Abs e) = abs $ evalPropExpr' d i e
 evalPropExpr' d !i !(OpI Sign e) = signum $ evalPropExpr' d i e
@@ -82,17 +82,23 @@ evalPropExpr' d !i !(OpII Mult l r) = evalPropExpr' d i l *  evalPropExpr' d i r
 evalPropExpr' d !i !(OpII Div l r)  = evalPropExpr' d i l ./ evalPropExpr' d i r
 evalPropExpr' d !i !(OpII Mod l r)  = evalPropExpr' d i l .% evalPropExpr' d i r
 evalPropExpr' d !i !(ChcI dim l r)
-  = ite (d dim) (evalPropExpr' d i l) (evalPropExpr' d i r)
+  = S.ite (d dim) (evalPropExpr' d i l) (evalPropExpr' d i r)
 
 -- | Generate a symbolic predicate for a feature expression.
-symbolicPropExpr :: (Show a, Ord a) => VProp a a -> Predicate
+symbolicPropExpr :: (Show a, Ord a) => VProp a a -> S.Predicate
 symbolicPropExpr e = do
     let vs = Set.toList (vars e)
         is = Set.toList (ivars e)
         ds = Set.toList (dimensions e)
-    syms  <- fmap (fromList . zip vs) (sBools (show <$> vs))
-    dims  <- fmap (fromList . zip ds) (sBools (map dimName ds))
-    isyms <- fmap (fromList . zip is) (sIntegers (show <$> is))
+        isType = Set.toList (ivarsWithType e)
+
+        helper :: (RefN, a) -> S.Symbolic SNum
+        helper (RefD, d) = S.sDouble (show d) >>= return . SD
+        helper (RefI, i) = S.sInteger (show i) >>= return . SI
+
+    syms  <- fmap (fromList . zip vs) (S.sBools (show <$> vs))
+    dims  <- fmap (fromList . zip ds) (S.sBools (map dimName ds))
+    isyms <- fmap (fromList . zip (show <$> is)) (traverse helper isType)
     let look f  = fromMaybe err  (lookup f syms)
         lookd d = fromMaybe errd (lookup d dims)
         looki i = fromMaybe erri (lookup i isyms)
@@ -104,7 +110,7 @@ symbolicPropExpr e = do
 -- | Perform andDecomposition, removing all choices from a proposition
 andDecomp :: (Show a) => (VProp a a) -> (Dim -> a) -> (VProp a a)
 andDecomp !(ChcB d l r) f = (dimToVar f d &&& andDecomp l f) |||
-                            (bnot (dimToVar f d) &&& andDecomp r f)
+                            (S.bnot (dimToVar f d) &&& andDecomp r f)
 andDecomp !(OpB op x)    f = OpB  op (andDecomp x f)
 andDecomp !(OpBB op l r) f = OpBB op (andDecomp l f) (andDecomp r f)
 andDecomp !(Opn op ps)   f = Opn  op (flip andDecomp f <$> ps)
