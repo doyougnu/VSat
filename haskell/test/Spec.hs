6f30f414c6b52d2e1308f891246aff70d214d320
doyougnu <jmy6342@gmail.com>
2018-07-31 14:40:50 -0700

    cleaning up Double explosion bug exploration

--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -6,6 +6,7 @@ import Run.Test
 
 main :: IO ()
 main = do defaultMain runProperties
-  where tests = testGroup "All" [ coreProperties
-                                , runProperties
+  where tests = testGroup "All" [ runProperties
+                                -- coreProperties
+                                -- , runProperties
                                 ]
