c79341b3cef595ca79c8e2df345e9a9907439101
doyougnu <jmy6342@gmail.com>
2018-08-09 14:35:36 -0700

    exposing solver configurations

--- haskell/src/Api.hs
+++ haskell/src/Api.hs
@@ -7,16 +7,24 @@ module Api ( SatResult(..)
 import Data.SBV (SatResult(..), ThmResult(..))
 
 import VProp.Types
+import Config (defConf,SMTConf(..))
 import Run
 import V
 import Utils (fst')
 
-sat :: [VProp String String -> VProp String String]
-  -> VProp String String
-  -> IO [V String (Maybe SatResult)]
-sat os p = fmap (bimap id (fmap SatResult)) . unbox . fst' <$> runVSMT os p
 
-prove :: [VProp String String -> VProp String String]
-  -> VProp String String
-  -> IO [V String (Maybe ThmResult)]
-prove os p = fmap (bimap id (fmap ThmResult)) . unbox . fst' <$> runVSMT os p
+-- | Run VSMT and return variable bindings
+sat :: VProp String String -> IO [V String (Maybe SatResult)]
+sat = satWith defConf
+
+-- | prove a proposition and return a counter example if it exists
+prove :: VProp String String -> IO [V String (Maybe ThmResult)]
+prove = proveWith defConf
+
+satWith :: SMTConf String -> VProp String String -> IO [V String (Maybe SatResult)]
+satWith conf p = fmap (bimap id (fmap SatResult)) . unbox . fst'
+                 <$> runVSMT conf p
+
+proveWith :: SMTConf String -> VProp String String -> IO [V String (Maybe ThmResult)]
+proveWith conf p = fmap (bimap id (fmap ThmResult)) . unbox . fst'
+                   <$> runVSMT conf p
