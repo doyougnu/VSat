b5237695650e62416218b85dd47a7ea787801e46
doyougnu <jmy6342@gmail.com>
2018-08-09 12:46:56 -0700

    adding dead branch elimination

--- haskell/src/Opts.hs
+++ haskell/src/Opts.hs
@@ -1,11 +1,10 @@
 module Opts where
 
 import System.IO.Unsafe (unsafePerformIO)
-import Data.SBV (Predicate,SBool,Symbolic,isSatisfiable)
+import Data.SBV (isSatisfiable)
 
 import VProp.Types
 import VProp.SBV (SAT, toPredicate)
-import VProp.Core
 import Data.List (sort)
 
 -- | Given any arbritrary prop move any choices to the right
@@ -21,6 +20,19 @@ moveChcToRight' (OpII Add x@(ChcI _ _ _) r)  = OpII Add r x
 moveChcToRight' (OpII Mult x@(ChcI _ _ _) r) = OpII Mult r x
 moveChcToRight' x                            = x
 
+-- | Given any arbritrary prop move any choices to the left
+moveChcToLeft :: (Ord a, Ord b) => VProp a b -> VProp a b
+moveChcToLeft (OpIB op l r) = OpIB op (moveChcToLeft' l) (moveChcToLeft' r)
+moveChcToLeft (Opn op xs) = Opn op . reverse $ sort xs
+moveChcToLeft (OpBB XOr l x@(ChcB _ _ _)) = OpBB XOr x l
+moveChcToLeft (OpBB BiImpl l x@(ChcB _ _ _)) = OpBB Impl x l
+moveChcToLeft x = x
+
+moveChcToLeft' :: (Ord a) => VIExpr a -> VIExpr a
+moveChcToLeft' (OpII Add l x@(ChcI _ _ _))  = OpII Add x l
+moveChcToLeft' (OpII Mult l x@(ChcI _ _ _)) = OpII Mult x l
+moveChcToLeft' x                            = x
+
 -- | Given a VProp try to eliminate some terms based on simple rules
 shrinkProp :: (Show a, Ord a) => VProp a a -> VProp a a
 shrinkProp (OpB Not (OpB Not x)) = shrinkProp x
@@ -29,7 +41,14 @@ shrinkProp (Opn Or xs) = Opn Or $ filter (not . unsatisfiable) xs
 shrinkProp e
   | unsatisfiable e = false
   | tautology e     = true
+-- | The inner false branch is dead if d == d'
+shrinkProp (ChcB d (ChcB d' l' _) r)
+  | d == d' = ChcB d (shrinkProp l') (shrinkProp r)
+-- | mirrored case
+shrinkProp (ChcB d l (ChcB d' _ r'))
+  | d == d' = ChcB d (shrinkProp l) (shrinkProp r')
 shrinkProp x = x
+
 
 -- | Is the predicate satisfiable?
 satisfiable :: SAT b => b -> Bool
