2acc7b3abe7a0e02a7159ec430707db1a5b2dadd
doyougnu <jmy6342@gmail.com>
2018-08-15 13:48:38 -0700

    JSON serializing, pretty printing

--- haskell/src/Json.hs
+++ haskell/src/Json.hs
@@ -11,6 +11,7 @@ import Data.Aeson
 import V (V(..))
 import VProp.Types
 import VProp.Core
+import VProp.Gen
 
 instance ToJSON SMTResult where
   toJSON (Unsatisfiable _) = object [("isSat" :: Text) .= ("Unsatisfiable" :: Text)]
@@ -33,16 +34,20 @@ instance (Show d, Show a, ToJSON a, ToJSON d) => ToJSON (V a d) where
                                         ]
                                ]
 
+-- Just keeping this for reference on writing a manual instance if needed later
 -- | VIExpr instances for FromJSON
-instance FromJSON NPrim where
-  parseJSON = withObject "num" $ \x -> do
-    type' <- x .: "type"
-    case type' of
-      "I" -> I <$> x .: "value"
-      "D" -> D <$> x .: "value"
-      _   -> fail ("unknown numeric type: " ++ type')
+-- Test With: decode "{\"type\":\"I\", \"value\": 1000}" :: Maybe NPrim
+-- instance FromJSON NPrim where
+--   parseJSON = withObject "num" $ \x -> do
+--     type' <- x .: "type"
+--     case type' of
+--       "I" -> I <$> x .: "value"
+--       "D" -> D <$> x .: "value"
+--       _   -> fail ("unknown numeric type: " ++ type')
+
 
 instance FromJSON N_N
+instance FromJSON NPrim
 instance FromJSON B_B
 instance FromJSON NN_N
 instance FromJSON BB_B
@@ -50,4 +55,20 @@ instance FromJSON NN_B
 instance FromJSON RefN
 instance FromJSON Opn
 instance FromJSON Dim
+instance FromJSON Var
 instance FromJSON a => FromJSON (VIExpr a)
+instance (FromJSON a, FromJSON b) => FromJSON (VProp a b)
+
+instance ToJSON B_B
+instance ToJSON NN_N
+instance ToJSON N_N
+instance ToJSON NPrim
+instance ToJSON BB_B
+instance ToJSON NN_B
+instance ToJSON RefN
+instance ToJSON Opn
+instance ToJSON Dim
+instance ToJSON Var
+
+instance ToJSON a => ToJSON (VIExpr a)
+instance (ToJSON a, ToJSON b) => ToJSON (VProp a b)
