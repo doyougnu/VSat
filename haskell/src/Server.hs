9ff27af47a4c634ad4891c0d4ab1004869283652
doyougnu <jmy6342@gmail.com>
2018-09-01 10:46:16 -0700

    adding logging and timing to every prop

--- haskell/src/Server.hs
+++ haskell/src/Server.hs
@@ -1,24 +1,30 @@
 module Server where
 
 import Web.Spock hiding (Var)
-import Web.Spock.Config
 import Control.Monad.IO.Class (liftIO)
 import GHC.Generics (Generic)
 import Data.Aeson hiding (json)
 import Data.Maybe (fromMaybe)
 import Network.Wai.Middleware.RequestLogger
 
-import           Data.Aeson       hiding (json)
-import           Data.Monoid      ((<>))
-import           Data.Text        (Text, pack)
-import           GHC.Generics
+import Data.ByteString.Lazy (appendFile)
+import Data.ByteString.Char8 (pack)
+
+import Data.Csv as C
+import Prelude hiding (writeFile, appendFile)
+
+import Control.Concurrent (forkIO)
+import System.CPUTime
+import System.Timeout
+import Data.Time.Clock
 
 import Api
-import Json
-import V
+import Json()
+import V()
+import Opts
 import Config
-import VProp.Gen
-import VProp.Types
+import VProp.Types hiding (appendFile)
+import VProp.Core
 
 -- * Api is the type for the spock server. I leave each config parameter as Unit
 -- * because I do not need a backend only a server daemon
@@ -45,16 +51,18 @@ app = do
   post "sat" satHandler
   post "prove" proveHandler
 
--- TODO cleanup these types
+logfile :: FilePath
+logfile = "timing_desc_data.csv"
+
+-- TODO cleanup these types and refactor out the logging to middleware
 satWithHandler :: ActionCtxT () (WebStateM () () ()) b
 satWithHandler = do
   req <- jsonBody' :: ApiAction (Request Var Var)
-  liftIO . putStrLn $ "receive: " ++ show req
   let prop = proposition req
       sets = fromMaybe defSettings (settings req)
       conf = toConf sets
-  liftIO . putStrLn $ "set: " ++ show sets
-  res <- liftIO $ satWith conf (bimap show show prop)
+  (runtime, res) <- liftIO . timeProc . satWith conf $ bimap show show prop
+  _ <- liftIO . forkIO $ logData prop sets runtime logfile
   json res
 
 proveWithHandler :: ActionCtxT () (WebStateM () () ()) b
@@ -63,19 +71,68 @@ proveWithHandler = do
   let prop = proposition req
       sets = maybe defSettings id (settings req)
       conf = toConf sets
-  res <- liftIO $ proveWith conf (bimap show show prop)
+  (runtime, res) <- liftIO . timeProc . proveWith conf $ bimap show show prop
+  _ <- liftIO . forkIO $ logData prop sets runtime logfile
   json res
 
 satHandler :: ActionCtxT () (WebStateM () () ()) b
 satHandler = do
   req <- jsonBody' :: ApiAction (Request Var Var)
   let prop = proposition req
-  res <- liftIO $ sat (bimap show show prop)
+  (runtime, res) <- liftIO . timeProc . sat $ bimap show show prop
+  _ <- liftIO . forkIO $ logData prop defSettings runtime logfile
   json res
 
 proveHandler :: ActionCtxT () (WebStateM () () ()) b
 proveHandler = do
   req <- jsonBody' :: ApiAction (Request Var Var)
   let prop = proposition req
-  res <- liftIO $ prove (bimap show show prop)
+  (runtime, res) <- liftIO . timeProc . prove $ bimap show show prop
+  _ <- liftIO . forkIO $ logData prop defSettings runtime logfile
   json res
+
+-- * Time logging functions
+
+-- | Required field namings for cassava csv library
+data RunData = RunData { utc_            :: !UTCTime
+                       , settings_       :: !Settings
+                       , runTime_        :: !Double
+                       , numTerms_       :: !Integer
+                       , numChc_         :: !Integer
+                       , numPlain_       :: !Integer
+                       , numSharedDims_  :: !Integer
+                       , numSharedPlain_ :: !Integer
+                       , maxShared_      :: !Integer
+                       } deriving (Generic, Show)
+
+instance C.ToField UTCTime  where toField = pack . show
+instance C.ToField Opts     where toField = pack . show
+instance C.ToField Solver   where toField = pack . show
+instance C.ToField Settings where toField = pack . show
+instance C.ToField [Opts]   where toField = mconcat . fmap (pack . show)
+instance C.ToRecord RunData
+instance C.ToRecord Settings
+instance C.ToRecord Opts
+
+logData :: VProp Var Var -> Settings -> Double -> FilePath -> IO ()
+logData prop sets runTime fn =
+  do time <- getCurrentTime
+     let row = RunData time sets runTime s c p sd sp ms
+     appendFile fn . C.encode $ pure row
+  where descriptorsFs = [ numTerms
+                        , numChc
+                        , numPlain
+                        , numSharedDims
+                        , numSharedPlain
+                        , toInteger . maxShared
+                        ]
+        [s,c,p,sd,sp,ms] = descriptorsFs <*> pure prop
+
+timeProc :: IO b -> IO (Double, b)
+timeProc !a = do
+  start <- getCPUTime
+  v <- a
+  end' <- timeout 300000000 (v `seq` getCPUTime)
+  let end = maybe (300 * 10^12) id end'
+      diff = (fromIntegral (end - start)) / (10 ^ 12)
+  return (diff,  v)
