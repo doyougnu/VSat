eba054d8115acf9076d4960885be8e7157da00ee
doyougnu <jmy6342@gmail.com>
2018-11-22 09:57:53 -0800

    forgot to add lang file

---
+++ haskell/src/CaseStudy/Auto/Lang.hs
@@ -0,0 +1,20 @@
+module CaseStudy.Auto.Lang where
+
+data AutoLang a = AutoLit Bool
+                | AutoRef a
+                | AutoNot (AutoLang a)
+                | BBinary BOp (AutoLang a) (AutoLang a)
+                | RBinary RBOp (ALang a) (ALang a)
+                deriving (Show)
+
+data BOp = And | Or | Impl | Eqv | Xor deriving Show
+data RBOp = GRT | GRTE | EQL | LST | LSTE  | NEQL deriving Show
+
+data ALang a = ALit Integer
+             | AVar a
+             | Ctx
+             | Neg (ALang a)
+             | ABinary AOp (ALang a) (ALang a)
+             deriving Show
+
+data AOp = Add | Subtract | Multiply | Divide | Modulus deriving Show
