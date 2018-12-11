283fc3a5069810796270c550917f96298e7455ba
doyougnu <jmy6342@gmail.com>
2018-12-11 13:13:24 -0800

    adding benchmark module for auto dataset

---
+++ haskell/bench/AutoBench.hs
@@ -0,0 +1,35 @@
+import qualified Data.ByteString as BS (readFile)
+import Text.Megaparsec (parse)
+import Data.Either (rights)
+import Data.Text (unpack)
+import Data.Bifunctor (bimap)
+import Data.Map.Internal.Debug (showTree)
+import Control.Arrow (first, second)
+
+import CaseStudy.Auto.Auto
+import CaseStudy.Auto.Parser (langParser)
+import VProp.Core
+import VProp.Types
+import Api
+import V
+
+import Data.Aeson (decodeStrict)
+
+autoFile :: FilePath
+autoFile = "src/CaseStudy/Auto/Automotive02_merged_evolution_history_integer.json"
+
+-- main :: IO (V String (Maybe ThmResult))
+main = do
+  jsn <- BS.readFile autoFile
+  let (Just auto) = decodeStrict jsn :: Maybe Auto
+      cs = constraints auto
+      ps' = parse langParser "" <$> cs
+      ps = rights ps'
+      prop = bimap unpack unpack <$> autoToVSat $ conjoin ps
+  -- res <- sat . flatten $ prop
+  print $ dimensions $ flatten prop
+  -- print res
+  -- return res
+  -- mapM (putStrLn . show) $ (second flatten) <$> (zip [1..] $ prop)
+  -- print $ flatten . last . take 10 . drop 1210 $ prop
+  -- putStrLn . showTree . fst . snd $ prop'
