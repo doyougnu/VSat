4ba667803fcf5d83784a928784c80b53430164f2
doyougnu <jmy6342@gmail.com>
2019-01-01 20:30:15 -0800

    conf compiles

--- haskell/src/Config.hs
+++ haskell/src/Config.hs
@@ -13,9 +13,9 @@ data Settings = Settings { solver :: Solver
                          , seed :: Maybe Integer
                          } deriving (Show,Generic)
 
-data SMTConf a = SMTConf { conf :: SMTConfig
-                         , opts :: [VProp a a -> VProp a a]
-                         }
+data SMTConf d a b = SMTConf { conf :: SMTConfig
+                             , opts :: [VProp d a b -> VProp d a b]
+                             }
 
 data Solver = Z3
             | Yices
@@ -26,7 +26,7 @@ data Solver = Z3
             deriving (Show,Generic)
 
 -- | Convert an interfacial interface to an SMT one
-toConf :: (Ord a, Show a) => Settings -> SMTConf a
+toConf :: (Show a,Show d,Show b,Ord a,Ord b,Ord d) => Settings -> SMTConf d a b
 toConf Settings{..} = foldr' ($!) emptyConf ss
   where ss = [setSeed seed, setSolver solver, setOpts optimizations]
 
@@ -50,35 +50,35 @@ debugSettings = Settings{ solver=Z3
                         , optimizations=[MoveRight,Atomize,CNF]
                         , seed=Nothing}
 
-defConf :: (Ord a,Show a) => SMTConf a
+defConf :: (Show a,Show d,Show b,Ord a,Ord b,Ord d) => SMTConf d a b
 defConf = toConf defSettings
 
-emptyConf :: (Show a, Ord a) => SMTConf a
+emptyConf :: (Show a,Show d,Show b,Ord a,Ord b,Ord d) => SMTConf d a b
 emptyConf = SMTConf{conf=z3, opts=[]}
 
-debugConf :: (Ord a,Show a) => SMTConf a
+debugConf :: (Show a,Show d,Show b,Ord a,Ord b,Ord d) => SMTConf d a b
 debugConf = toConf debugSettings
 
-minConf :: (Ord a,Show a) => SMTConf a
+minConf ::(Show a,Show d,Show b,Ord a,Ord b,Ord d) => SMTConf d a b
 minConf = toConf minSettings
 
-allOptsConf :: (Ord a,Show a) => SMTConf a
+allOptsConf ::(Show a,Show d,Show b,Ord a,Ord b,Ord d) => SMTConf d a b
 allOptsConf = toConf allOptsSettings
 
 -- | apply some function on the solver options. This could be done more cleanly
 -- with lenses but I don't want to bloat the library
-addOption :: ([SMTOption] -> [SMTOption]) -> SMTConf a -> SMTConf a
+addOption :: ([SMTOption] -> [SMTOption]) -> SMTConf d a b -> SMTConf d a b
 addOption f c = SMTConf {conf = c'{solverSetOptions=f sOpts}, opts = os}
   where c' = conf c
         sOpts = solverSetOptions c'
         os = opts c
 
 -- | set the seed of the internal solver
-setSeed :: (Maybe Integer) -> SMTConf a -> SMTConf a
+setSeed :: (Maybe Integer) -> SMTConf d a b -> SMTConf d a b
 setSeed (Just x) c = addOption ((RandomSeed x):) c
 setSeed Nothing  c = c
 
-setSolver :: Solver -> SMTConf a -> SMTConf a
+setSolver :: Solver -> SMTConf d a b -> SMTConf d a b
 setSolver Z3 a        = a{conf=z3}
 setSolver Yices a     = a{conf=yices}
 setSolver MathSat a   = a{conf=mathSAT}
@@ -86,10 +86,12 @@ setSolver Boolector a = a{conf=boolector}
 setSolver Abc a       = a{conf=abc}
 setSolver Cvc4 a      = a{conf=cvc4}
 
-setOpts :: (Ord a, Show a) => [Opts] -> SMTConf a -> SMTConf a
+setOpts :: (Ord a,Ord b,Ord d,Show d,Show b,Show a) =>
+  [Opts] -> SMTConf d a b -> SMTConf d a b
 setOpts os c = c{opts=convertOpts <$> os}
 
-convertOpts :: (Ord a,Show a) =>  Opts -> VProp a a -> VProp a a
+convertOpts :: (Ord a,Ord b,Ord d,Show d,Show a,Show b) =>
+  Opts -> VProp d a b -> VProp d a b
 convertOpts MoveRight = moveChcToRight
 convertOpts MoveLeft  = moveChcToLeft
 convertOpts Shrink    = shrinkProp
