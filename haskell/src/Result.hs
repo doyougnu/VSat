8e71945556ea5842d9e2c1ffb2fe906dd631d718
doyougnu <jmy6342@gmail.com>
2019-02-12 21:59:12 -0800

    enabling profiling, more result refactoring

--- haskell/src/Result.hs
+++ haskell/src/Result.hs
@@ -1,24 +1,21 @@
 module Result ( Result (..)
-              , UniformProp (..)
+              , ResultProp (..)
               , Resultable
               , AssocList
-              , addToResult
+              , insertToResult
+              , insertToSat
               , lookupRes
               , lookupRes_
               , getResSat
               , isDMNull
-              , modifySat
-              , assocToResult
               , configToUniProp
-              , IsString
-              , Hashable
               , getResult
+              , consResultProp
               ) where
 
-import           Control.Arrow      (second, (***))
-import           Data.Hashable      (Hashable)
-import qualified Data.HashMap.Lazy  as HM
-import           Data.Map           (toList)
+import           Control.DeepSeq    (NFData)
+import           Data.Sequence
+import qualified Data.Map           as M
 import           Data.Maybe         (maybe)
 import           Data.SBV           (SMTResult (..), defaultSMTCfg,
                                      getModelDictionary)
@@ -30,7 +27,7 @@ import           Data.Text          (Text)
 import           GHC.Generics       (Generic)
 
 import           VProp.Core         (configToProp)
-import           VProp.Types        (Config, VProp, Var, false, (|||))
+import           VProp.Types        (Config, VProp(..), Var)
 
 -- | a type synonym that represents SBVs return type for getAssocs
 type AssocList = [(String, Bool)]
@@ -38,64 +35,67 @@ type AssocList = [(String, Bool)]
 -- | A custom type whose only purpose is to define a monoid instance over VProp
 -- with logical or as the concat operation and false as unit. We constrain all
 -- variable references to be the same just for the Result type
-newtype UniformProp d = UniformProp {getProp :: VProp d d d}
+newtype ResultProp d = ResultProp {getProp :: Seq (VProp d d d)}
   deriving (Show, Eq, Generic)
 
-instance Semigroup (UniformProp d) where
-  (<>) x y = UniformProp $ (getProp x) ||| (getProp y)
+consResultProp :: VProp d d d -> ResultProp d -> ResultProp d
+consResultProp x xs = ResultProp $ x <| (getProp xs)
 
-instance Monoid (UniformProp d) where
-  mempty  = UniformProp false
+instance NFData d => NFData (ResultProp d)
+
+instance Semigroup (ResultProp d) where
+  (<>) x y = ResultProp $ (getProp x) >< (getProp y)
+
+instance Monoid (ResultProp d) where
+  mempty  = ResultProp empty
   mappend = (<>)
 
-instance Hashable Var
 instance Resultable Var
 instance Resultable String
 instance Resultable Text
 
 -- | a type class synonym for constraints required to produce a result
-class (IsString a, Eq a, Hashable a) => Resultable a
+class (IsString a, Eq a, Ord a) => Resultable a
 
 -- | a result is a map of propositions where SAT is a boolean formula on
 -- dimensions, the rest of the keys are variables of the prop to boolean
 -- formulas on dimensions that dictate the values of those variables be they
 -- true or false
-newtype Result d = Result {getRes :: HM.HashMap d (UniformProp d)}
+newtype Result d = Result {getRes :: M.Map d (ResultProp d)}
   deriving (Show,Eq,Generic)
 
-instance (Eq d, Hashable d) => Semigroup (Result d) where
-  x <> y = Result $ HM.unionWith (<>) (getRes x) (getRes y)
+instance NFData d => NFData (Result d)
+
+instance (Eq d, Ord d) => Semigroup (Result d) where
+  x <> y = Result $ M.unionWith (<>) (getRes x) (getRes y)
 
 -- | we define a special key "__Sat" to represent when all dimensions are
 -- satisfiable. TODO use type families to properly abstract the key out
 instance (Resultable d) => Monoid (Result d) where
-  mempty  = Result $ HM.singleton "__Sat" mempty
+  mempty  = Result $ M.singleton "__Sat" mempty
   mappend = (<>)
 
 
-configToUniProp :: Config d -> UniformProp d
-configToUniProp = UniformProp . configToProp
+configToUniProp :: Config d -> ResultProp d
+configToUniProp = ResultProp . pure . configToProp
 
-addToResult :: (Eq d, Hashable d) => d -> UniformProp d -> Result d -> Result d
-addToResult k v = Result . HM.insertWith (<>) k v . getRes
+insertToResult :: (Eq d, Ord d) => d -> ResultProp d -> Result d -> Result d
+insertToResult k v = Result . M.insertWith (<>)  k v . getRes
 
-lookupRes :: (Eq d, Hashable d) => d -> Result d -> Maybe (UniformProp d)
-lookupRes k res = HM.lookup k (getRes res)
+insertToSat :: Resultable d => ResultProp d -> Result d -> Result d
+insertToSat = insertToResult "__Sat"
 
-lookupRes_ :: (Eq d, Hashable d) => d -> Result d -> UniformProp d
-lookupRes_ k res = (HM.!) (getRes res) k
+lookupRes :: (Eq d, Ord d) => d -> Result d -> ResultProp d
+lookupRes k res = maybe mempty id $ M.lookup k (getRes res)
 
-getResSat :: Resultable d => Result d -> UniformProp d
+lookupRes_ :: (Eq d, Ord d) => d -> Result d -> ResultProp d
+lookupRes_ k res = (M.!) (getRes res) k
+
+getResSat :: Resultable d => Result d -> ResultProp d
 getResSat = lookupRes_ "__Sat"
 
 isDMNull :: Resultable d => Result d -> Bool
-isDMNull = maybe False ((==) mempty) . lookupRes "__Sat"
-
-modifySat :: Resultable d => UniformProp d -> Result d -> Result d
-modifySat = addToResult "__Sat"
-
-assocToResult :: Resultable d => (Bool -> UniformProp d) -> AssocList -> Result d
-assocToResult f xs = Result . HM.fromList $ fmap (fromString *** f) xs
+isDMNull = M.null . getRes
 
 getVSMTModel :: Query (Maybe SMTResult)
 getVSMTModel = do cs <- checkSat
@@ -107,6 +107,9 @@ getVSMTModel = do cs <- checkSat
                                 Just $ Unsatisfiable defaultSMTCfg Nothing
                     Sat   -> getSMTResult >>= return . pure
 
-getResult :: Resultable d => (Bool -> UniformProp d) -> Query (Result d)
-getResult f = do as <- (maybe mempty id . fmap getModelDictionary) <$> getVSMTModel
-                 return $ assocToResult f (second cwToBool <$> toList as)
+getResult :: Resultable d => (Bool -> ResultProp d) -> Query (Result d)
+getResult f =
+  do as <- (maybe mempty id . fmap getModelDictionary) <$> getVSMTModel
+     return $
+       Result (M.foldMapWithKey
+               (\k a -> M.singleton (fromString k) (f $ cwToBool a)) as)
