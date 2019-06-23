15b4caac00b27efd9e1c947af1fb921af62377b7
doyougnu <jmy6342@gmail.com>
2019-06-22 17:55:31 -0700

    new auto data after bug fixes

--- haskell/julia/timing.R
+++ haskell/julia/timing.R
@@ -1,12 +1,28 @@
 library(ggplot2)
+library(dplyr)
 
-timingsResultsFile <- "../data/data.csv"
+timingsResultsFile <- "../data/findata.csv"
 
 data <- read.csv(file=timingsResultsFile)
 
-ggplot(data, mapping = aes(x=PlainCount, y=Mean, shape=Algorithm, color=Algorithm, group=Algorithm)) +
+df <- data %>% filter(CompressionRatio > 0) %>% filter( Config != "V1"
+                                                      ,Config != "V2"
+                                                      ,Config != "V3"
+                                                      ,Config != "V4"
+                                                      ,Config != "V5"
+                                                      ,Config != "V6"
+                                                      ,Config != "V7"
+                                                      ,Config != "V8"
+                                                      ,Config != "V9"
+                                                      ,Config != "V10"
+                                                      ,Config != "EvolutionAware")
+
+ggplot(df, mapping = aes(x=(CompressionRatio * 100), y=Mean, shape=Algorithm, fill=Algorithm)) +
   theme(axis.text.x = element_text(angle = 90)) +
-  scale_x_continuous(trans='log10') + geom_point() + geom_line()
+  scale_y_log10() +
+  scale_x_log10() +
+  geom_point() + geom_smooth(method = lm, se = FALSE)
+  ## geom_col(position = "dodge")
 ## +
 ##   facet_grid(Config ~ .)
 
