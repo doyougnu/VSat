e48ace13d45daa7db9173a62a2b7c42f7d7900e2
doyougnu <jmy6342@gmail.com>
2019-07-11 08:12:17 -0700

    R scripts setup

--- haskell/julia/autoPlotting.R
+++ haskell/julia/autoPlotting.R
@@ -3,9 +3,9 @@ library(dplyr)
 
 timingsResultsFile <- "../data/auto_data.csv"
 
-data <- read.csv(file=timingsResultsFile)
+data <- read.csv(file=timingsResultsFile) %>% mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config))
 
-df <- data %>% filter( Config != "V2"
+dfCascade <- data %>% filter( Config != "V2"
                       ,Config != "V3"
                       ,Config != "V4"
                       ,Config != "V5"
@@ -17,15 +17,28 @@ df <- data %>% filter( Config != "V2"
                       ,Config != "EvolutionAware") %>% filter (Algorithm != "p-->p"
                                                              , Algorithm != "p-->v")
 
-## df <- data %>% filter( Config != "V1*V2"
-##                       ,Config != "V1*V2*V3"
-##                       ,Config != "V1*V2*V3*V4"
-##                       ,Config != "V1*V2*V3*V4*V5"
-##                       ,Config != "V1*V2*V3*V4*V5*V6"
-##                       ,Config != "V1*V2*V3*V4*V5*V6*V7"
-##                       ,Config != "V1*V2*V3*V4*V5*V6*V7*V8"
-##                       ,Config != "V1*V2*V3*V4*V5*V6*V7*V8*V9"
-##                       ,Config != "V1*V2*V3*V4*V5*V6*V7*V8*V9*V10")
+sumData <- data %>% filter( Config != "V1*V2"
+                          ,Config != "EvolutionAware"
+                          ,Config != "V1*V2*V3"
+                          ,Config != "V1*V2*V3*V4"
+                          ,Config != "V1*V2*V3*V4*V5"
+                          ,Config != "V1*V2*V3*V4*V5*V6"
+                          ,Config != "V1*V2*V3*V4*V5*V6*V7"
+                          ,Config != "V1*V2*V3*V4*V5*V6*V7*V8"
+                          ,Config != "V1*V2*V3*V4*V5*V6*V7*V8*V9"
+                          ,Config != "V1*V2*V3*V4*V5*V6*V7*V8*V9*V10") %>% group_by(Algorithm) %>%
+  summarise(Mean = sum(Mean)) %>% mutate(Config = as.factor("Sum"))
+
+df <- merge(sumData, data, all=TRUE) %>%
+  filter( Config != "V1*V2"
+         ,Config != "V1*V2*V3"
+         ,Config != "V1*V2*V3*V4"
+         ,Config != "V1*V2*V3*V4*V5"
+         ,Config != "V1*V2*V3*V4*V5*V6"
+         ,Config != "V1*V2*V3*V4*V5*V6*V7"
+         ,Config != "V1*V2*V3*V4*V5*V6*V7*V8"
+         ,Config != "V1*V2*V3*V4*V5*V6*V7*V8*V9"
+         ,Config != "V1*V2*V3*V4*V5*V6*V7*V8*V9*V10")
 
 facetLabels <- c(V1 = "V1"
                , "V1*V2" = "V1**V2"
@@ -38,12 +51,20 @@ facetLabels <- c(V1 = "V1"
                , "V1*V2*V3*V4*V5*V6*V7*V8*V9"     = "V1...**...V9"
                , "V1*V2*V3*V4*V5*V6*V7*V8*V9*V10" = "V1...**...V10")
 
-ggplot(df, mapping = aes(x=Algorithm, y=Mean, shape=Algorithm, fill=Algorithm)) +
+cascade_plt <- ggplot(dfCascade, mapping = aes(x=Algorithm, y=Mean, shape=Algorithm, fill=Algorithm)) +
   theme(axis.text.x = element_text(angle = 90)) +
-  ## scale_y_log10() +
-  ## scale_x_log10() +
-  ## geom_point() + geom_smooth(method = lm, se = FALSE)
   geom_col(position = "dodge") +
-  facet_grid(. ~ Config, labeller = labeller(Config = facetLabels)) + theme(strip.text.x = element_text(size=10))
+  facet_grid(. ~ Config
+           , labeller = labeller(Config = facetLabels)
+             ) + theme(strip.text.x = element_text(size=10))
 
-ggsave("../plots/fin_cascade.png",device = "png")
+ggsave("../plots/auto_cascade.png", plot = cascade_plt, device = "png")
+
+evo_plt <- ggplot(df, mapping = aes(x=Algorithm, y=Mean, shape=Algorithm, fill=Algorithm)) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  geom_col(position = "dodge") +
+  facet_grid(. ~ Config
+           ## , labeller = labeller(Config = facetLabels)
+             ) + theme(strip.text.x = element_text(size=10))
+
+ggsave("../plots/auto_evo.png", plot = evo_plt, device = "png")
