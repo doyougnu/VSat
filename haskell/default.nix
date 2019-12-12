8916c3db5c5f2f1841e53ed61f2e9d4e60de4d7c
doyougnu <jmy6342@gmail.com>
2019-12-12 12:55:00 -0800

    adding nix files

---
+++ haskell/default.nix
@@ -0,0 +1,39 @@
+{ mkDerivation, aeson, aeson-pretty, base, bytestring, cassava
+, containers, criterion, deepseq, genifunctors, hashable, hpack
+, megaparsec, mtl, QuickCheck, sbv, Spock, stdenv, tasty
+, tasty-hspec, tasty-hunit, tasty-quickcheck, tasty-smallcheck
+, text, time, unicode-show, unordered-containers, vector, wai-extra
+}:
+mkDerivation {
+  pname = "vsat";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    aeson aeson-pretty base bytestring cassava containers deepseq
+    genifunctors hashable megaparsec mtl sbv Spock tasty-quickcheck
+    text time unicode-show unordered-containers vector wai-extra
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    aeson aeson-pretty base bytestring cassava containers deepseq
+    genifunctors hashable megaparsec mtl sbv Spock tasty-quickcheck
+    text time unicode-show unordered-containers vector wai-extra
+  ];
+  testHaskellDepends = [
+    aeson aeson-pretty base bytestring cassava containers deepseq
+    genifunctors hashable megaparsec mtl QuickCheck sbv Spock tasty
+    tasty-hspec tasty-hunit tasty-quickcheck tasty-smallcheck text time
+    unicode-show unordered-containers vector wai-extra
+  ];
+  benchmarkHaskellDepends = [
+    aeson aeson-pretty base bytestring cassava containers criterion
+    deepseq genifunctors hashable megaparsec mtl sbv Spock
+    tasty-quickcheck text time unicode-show unordered-containers vector
+    wai-extra
+  ];
+  preConfigure = "hpack";
+  homepage = "https://github.com/doyougnu/vsat#readme";
+  license = stdenv.lib.licenses.bsd3;
+}
