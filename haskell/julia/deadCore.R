851599e823c5eb5219796abe19e606a10c8efbc7
doyougnu <jmy6342@gmail.com>
2020-05-02 10:06:41 +0200

    dead core plotting done

--- haskell/julia/deadCore.R
+++ haskell/julia/deadCore.R
@@ -16,29 +16,38 @@ autoDF <- autoData %>% mutate(data = "Auto") # %>% select(Mean, Algorithm, Compr
 
 data <- rbind(finDF, autoDF)
 
-deadCoreDF <- data %>% filter(Variants > 2) %>% group_by(Algorithm) %>% arrange(Variants)
+deadCoreDF <- data %>%
+  group_by(data) %>%
+  arrange(Mean) %>%
+  mutate(MeanLbl = case_when(data == "Auto" ~ signif(Mean, 5),
+                             data == "Fin" ~ signif(Mean, 3)))
 
-dc <- ggplot(deadCoreDF, mapping = aes(x=Variants, y=Mean, shape=Algorithm, color=Algorithm)) +
-  ylab("Mean [s]") +
-  geom_point(size=3) +
-  geom_line() +
-  scale_shape_manual(values = c(1,2,5,17)) +
-  scale_x_continuous(breaks=seq(2, 2048, 8)) +
-  facet_wrap(. ~ data, scales = "free") +
+dc <- ggplot(deadCoreDF
+           , mapping = aes(x=Algorithm
+                         , y=Mean
+                         , color = Algorithm)) +
   theme_classic() +
-  ggtitle("Dead Core Demonstration") +
-  ylab("Time [s] to solve all Variants") +
-  theme(legend.position = "bottom")
+  guides(fill = FALSE) +
+  guides(size = FALSE) +
+  guides(color = FALSE) +
+  geom_bar(stat="identity", fill="white", width=0.5
+           ,size=2) +
+  geom_text(aes(label=MeanLbl, hjust=1.4), color="black") +
+  facet_wrap(. ~ data, scales = "free_x") +
+  labs(title = "Dead Core Demonstration",
+       y = "Time [s] to solve Dead and Core Analysis over all Version") +
+  theme(legend.position = "none") +
+  coord_flip()
 
-## ggsave("../plots/DeadCore.png", plot = dc, height = 4, width = 7, device = "png")
+ggsave("../plots/DeadCore.png", plot = dc, height = 4, width = 7, device = "png")
 
-dcAuto <- ggplot(deadCoreDF %>% filter(data =="Auto"), mapping = aes(x=Variants, y=Mean, shape=Algorithm, color=Algorithm)) +
-  ylab("Mean [s]") +
-  geom_point(size=3) +
-  geom_line() +
-  scale_shape_manual(values = c(1,2,5,17)) +
-  scale_x_continuous(breaks=seq(0, 32, 8)) +
-  theme_classic() +
-  ggtitle("Dead Core Demonstration") +
-  ylab("Time [s] to solve all Variants") +
-  theme(legend.position = "bottom")
+## dcAuto <- ggplot(deadCoreDF %>% filter(data =="Auto"), mapping = aes(x=Algorithm, y=Mean, shape=Algorithm, color=Algorithm)) +
+##   ylab("Mean [s]") +
+##   geom_point(size=3) +
+##   geom_line() +
+##   scale_shape_manual(values = c(1,2,5,17)) +
+##   scale_x_continuous(breaks=seq(0, 32, 8)) +
+##   theme_classic() +
+##   ggtitle("Dead Core Demonstration") +
+##   ylab("Time [s] to solve all Variants") +
+##   theme(legend.position = "bottom")
