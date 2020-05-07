2070550af06f7e2d04c0ba1a43b1e91a28bb5653
doyougnu <jmy6342@gmail.com>
2020-05-07 10:48:16 +0200

    stats

--- haskell/julia/plotting.R
+++ haskell/julia/plotting.R
@@ -8,6 +8,7 @@ library(Hmisc)
 finResultsFile <- "../data/fin_data.csv"
 autoResultsFile <- "../data/auto_data.csv"
 finRawFile <- "../data/fin_raw_singletons.csv"
+autoRawFile <- "../data/auto_raw_singletons.csv"
 
 finData <- read.csv(file=finResultsFile) %>% mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config)) %>% mutate(Algorithm = gsub("-->", "\U27f6", Algorithm))
 
@@ -34,24 +35,30 @@ rq1 <- ggplot(rq1DF, mapping = aes(x=Variants, y=Mean, shape=Algorithm, color=Al
 
 ################# Singleton Analysis ##############################
 
-finSingData <- read.csv(file=finRawFile) %>% mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config)) %>% mutate(Algorithm = gsub("-->", "\U27f6", Algorithm), data = "Financial")
+finSingData <- read.csv(file=finRawFile) %>% mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config)) %>% mutate(Algorithm = gsub("-->", "\U27f6", Algorithm), data = "Financial") %>% group_by(Algorithm, Config) %>% mutate(TimeCalc = time -append(0,head(time, -1))) %>% filter(TimeCalc > 0)
 
-rq3DF <- finSingData %>%
-  group_by(Algorithm, Config) %>%
-  mutate(TimeCalc = time -append(0,head(time, -1))) %>%
-  filter(TimeCalc > 0) ## remove the filter after running on machine with proper
-                       ## memory
+## remove extreme outliers
+finSingData <- finSingData[-c(45,62,202),]
+## memory
 
-rq3 <- ggplot(rq3DF, aes(x=Algorithm, y=TimeCalc, shape=Algorithm, color=Algorithm)) +
-  geom_violin(trim=FALSE) +
-  geom_jitter() +
+autoSingData <- read.csv(file=autoRawFile) %>% mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config)) %>% mutate(Algorithm = gsub("-->", "\U27f6", Algorithm), data = "Auto") %>% group_by(Algorithm, Config) %>% mutate(TimeCalc = time -append(0,head(time, -1))) %>% filter(TimeCalc > 0)
+
+rq3DF <- rbind(finSingData, autoSingData)
+
+## previous TUKEY-HSD
+
+rq3 <- ggplot(rq3DF, aes(x=Algorithm, y=TimeCalc, fill=Config)) +
+  ## geom_violin(trim=FALSE) +
+  geom_boxplot(outlier.color="red", outlier.shape=16) +
+  geom_jitter(alpha=0.5) +
+  facet_wrap(.~ data, scales="free") +
   theme_classic() +
-  scale_shape_manual(values = c(1,2,5,17)) +
-  stat_summary(fun.data="mean_sdl"
-             , fun.args = list(mult=2)
-               , geom="pointrange"
-               , color="black"
-               , size=0.65) +
+  ## scale_shape_manual(values = c(1,2,5,17)) +
+  ## stat_summary(fun.data="mean_sdl"
+  ##            , fun.args = list(mult=2)
+  ##              , geom="pointrange"
+  ##              , color="black"
+  ##              , size=0.65) +
   ggtitle("RQ3: Overhead of Variational Solving on Plain Formulas") +
   ylab("Time [s]") +
   theme(legend.position = "none")
@@ -60,51 +67,45 @@ rq3 <- ggplot(rq3DF, aes(x=Algorithm, y=TimeCalc, shape=Algorithm, color=Algorit
 ggsave("../plots/RQ3.png", plot = rq3, height = 4, width = 7, device = "png")
 
 ### Do a two-way anova looking at both algorithm and config and their interaction
-res.aov <- aov(TimeCalc ~ Algorithm * Config, data = rq3DF)
+res.fin.aov <- aov(TimeCalc ~ Algorithm * Config, data = finSingData)
 
 ### Check the summary to see what is significant, all of it is as expected
-sig_res <- summary(res.aov)
-
-## Df Sum Sq Mean Sq F value   Pr(>F)
-## Algorithm          3  10.28   3.428   9.516 4.71e-06 ***
-## Config             9  44.95   4.995  13.866  < 2e-16 ***
-## Algorithm:Config  27  16.64   0.616   1.711   0.0167 *
-##                                                 Residuals        344 123.91   0.360
-## ---
-##   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
+res.fin.sig <- summary(res.fin.aov)
 
 ### Finally, perform the pair-wise Tukey comparison to test the difference
 ### between groups
-tuk_res <- TukeyHSD(res.aov, which = "Algorithm")
+fin.tuk_res <- TukeyHSD(res.fin.aov, which = "Algorithm")
 
 ## Note that only v-->p and p-->p is not significant
 
 ## Tukey multiple comparisons of means
 ## 95% family-wise confidence level
 
-## Fit: aov(formula = TimeCalc ~ Algorithm * Config, data = rq3DF)
+## Fit: aov(formula = TimeCalc ~ Algorithm * Config, data = finSingData)
 
 ## $Algorithm
-##                diff         lwr         upr     p adj
-## p⟶v-p⟶p -0.24610722 -0.47102936 -0.02118509 0.0256691
-## v⟶p-p⟶p -0.02496165 -0.25159688  0.20167358 0.9919734
-## v⟶v-p⟶p  0.21014178 -0.01422963  0.43451319 0.0756077
-## v⟶p-p⟶v  0.22114557 -0.00193726  0.44422841 0.0530032
-## v⟶v-p⟶v  0.45624900  0.23546641  0.67703159 0.0000010
-## v⟶v-v⟶p  0.23510343  0.01257587  0.45763099 0.0337644
+##                diff        lwr        upr     p adj
+## p⟶v-p⟶p -0.02349029 -0.1497346 0.10275401 0.9633882
+## v⟶p-p⟶p -0.10479358 -0.2288089 0.01922176 0.1304058
+## v⟶v-p⟶p  0.39055360  0.2625286 0.51857858 0.0000000
+## v⟶p-p⟶v -0.08130330 -0.2053186 0.04271205 0.3291041
+## v⟶v-p⟶v  0.41404389  0.2860189 0.54206887 0.0000000
+## v⟶v-v⟶p  0.49534719  0.3695196 0.62117476 0.0000000
 
 ### Check leverage of the observations for outliers
-res.outliers <- plot(res.aov, 1)
+res.fin.outliers <- plot(res.fin.aov, 1)
 ### observations 251, 148 and 307 are severe outliers for this data
 
 
 ### Check the normality assumption of the ANOVA
-res.ass <- plot(res.aov, 2)
+res.fin.ass <- plot(res.fin.aov, 2)
 ### its bad enough to check the stats directly with the shapiro-wilk test
 
-aov_resids <- residuals(object=res.aov)
-res.shaps <- shapiro.test(x = aov_resids)
+## check residuals
+aov.fin.resids <- residuals(object=res.fin.aov)
+
+## perform Shapiro-wilk normality test
+res.fin.shaps <- shapiro.test(x = aov.fin.resids)
 
 ## Shapiro-Wilk normality test
 
@@ -112,3 +113,42 @@ res.shaps <- shapiro.test(x = aov_resids)
 ## W = 0.87101, p-value < 2.2e-16
 
 ### those outliers are too significant!!!
+
+############################# Auto ANOVA ##############################
+### Do a two-way anova looking at both algorithm and config and their interaction
+res.auto.aov <- aov(TimeCalc ~ Algorithm * Config, data = autoSingData)
+
+### Check the summary to see what is significant, all of it is as expected
+res.auto.sig <- summary(res.auto.aov)
+
+### Autoally, perform the pair-wise Tukey comparison to test the difference
+### between groups
+auto.tuk_res <- TukeyHSD(res.auto.aov, which = "Algorithm")
+
+## Note that only v-->p and p-->p is not significant
+
+## Tukey multiple comparisons of means
+## 95% family-wise confidence level
+
+## Fit: aov(formula = TimeCalc ~ Algorithm * Config, data = autoSingData)
+
+## $Algorithm
+##                diff        lwr        upr     p adj
+## p⟶v-p⟶p -0.02349029 -0.1497346 0.10275401 0.9633882
+## v⟶p-p⟶p -0.10479358 -0.2288089 0.01922176 0.1304058
+## v⟶v-p⟶p  0.39055360  0.2625286 0.51857858 0.0000000
+## v⟶p-p⟶v -0.08130330 -0.2053186 0.04271205 0.3291041
+## v⟶v-p⟶v  0.41404389  0.2860189 0.54206887 0.0000000
+## v⟶v-v⟶p  0.49534719  0.3695196 0.62117476 0.0000000
+
+### Check leverage of the observations for outliers
+res.auto.outliers <- plot(res.auto.aov, 1)
+
+
+### Check the normality assumption of the ANOVA
+res.auto.ass <- plot(res.auto.aov, 2)
+
+aov.auto.resids <- residuals(object=res.auto.aov)
+res.auto.shaps <- shapiro.test(x = aov.auto.resids)
+
+## Shapiro-Wilk normality test
