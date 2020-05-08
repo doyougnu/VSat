f4948ce75e4478c915bbf2802e33e79da32c289e
doyougnu <jmy6342@gmail.com>
2020-05-08 13:44:22 +0200

    even more stats

--- haskell/julia/plotting.R
+++ haskell/julia/plotting.R
@@ -9,23 +9,29 @@ finResultsFile <- "../data/fin_data.csv"
 autoResultsFile <- "../data/auto_data.csv"
 finRawFile <- "../data/fin_raw_singletons.csv"
 autoRawFile <- "../data/auto_raw_singletons.csv"
+finCountsFile <- "../data/fin_counts.csv"
+autoCountsFile <- "../data/auto_counts.csv"
 
 finData <- read.csv(file=finResultsFile) %>% mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config)) %>% mutate(Algorithm = gsub("-->", "\U27f6", Algorithm))
-
 autoData <- read.csv(file=autoResultsFile) %>% mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config)) %>% mutate(Algorithm = gsub("-->", "\U27f6", Algorithm))
 
 finDF <- finData %>% mutate(data = "Financial") # %>% select(Mean, Algorithm, CompressionRatio, data, ChcCount, PlainCount, Config)
 autoDF <- autoData %>% mutate(data = "Auto") # %>% select(Mean, Algorithm, CompressionRatio, data, ChcCount, PlainCount, Config)
 
+finCountData  <- read.csv(file=finCountsFile) %>% mutate(data = "Financial")
+autoCountData <- read.csv(file=autoCountsFile) %>% mutate(data = "Auto")
+countData     <- rbind(finCountData, autoCountData) %>% mutate(SatRatio = Sat / UnSat) %>% mutate(Sat = as.factor(Sat), UnSat = as.factor(UnSat))
+
 data <- rbind(finDF, autoDF)
 
-rq1DF <- data %>% filter(Variants > 2) %>% group_by(Algorithm) %>% arrange(Variants)
+rq1DF <- data %>% filter(Variants > 2) %>% group_by(Algorithm) %>% arrange(Variants) %>% merge(countData)
 
-rq1 <- ggplot(rq1DF, mapping = aes(x=Variants, y=Mean, shape=Algorithm, color=Algorithm)) +
-  geom_point(size=3) +
-  geom_line() +
+rq1 <- ggplot(rq1DF) +
+  geom_line(aes(x=Variants, y=Mean, color=Algorithm)) +
+  geom_point(aes(x=Variants, y=Mean, shape=Algorithm, color=Algorithm),size=3) +
   scale_shape_manual(values = c(1,2,5,17)) +
   facet_wrap(. ~ data, scales = "free") +
+  geom_bar(aes(x=Variants, fill=Sat*100,size=5)) +
   theme_classic() +
   ggtitle("RQ1: Performance as variants increase") +
   ylab("Time [Min.] to solve all Variants") +
@@ -53,7 +59,7 @@ rq3 <- ggplot(rq3DF, aes(x=Config, y=Mean, fill=Algorithm, shape=Algorithm, colo
   ggtitle("RQ3: Overhead of Variational Solving on Plain Formulas") +
   ylab("Time [s] to solve single version variant") +
   xlab("Feature Model Version") +
-  theme(legend.position = "bottom") +
+  theme(legend.position = "right") +
   theme(panel.grid.major.y = element_line(color = "grey")) +
   coord_flip()
 
@@ -67,9 +73,6 @@ finSingData <- read.csv(file=finRawFile) %>%
   group_by(Algorithm, Config) %>%
   mutate(TimeCalc = time -append(0,head(time, -1))) %>% filter(TimeCalc > 0)
 
-## remove extreme outliers
-finSingData <- finSingData[-c(45,62,202),]
-## memory
 
 autoSingData <- read.csv(file=autoRawFile) %>%
   mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config)) %>%
@@ -85,23 +88,20 @@ res.fin.sig <- summary(res.fin.aov)
 
 ### Finally, perform the pair-wise Tukey comparison to test the difference
 ### between groups
-fin.tuk_res <- TukeyHSD(res.fin.aov, which = "Algorithm")
-
-## Note that only v-->p and p-->p is not significant
-
-## Tukey multiple comparisons of means
-## 95% family-wise confidence level
-
-## Fit: aov(formula = TimeCalc ~ Algorithm * Config, data = finSingData)
-
-## $Algorithm
-##                diff        lwr        upr     p adj
-## p⟶v-p⟶p -0.02349029 -0.1497346 0.10275401 0.9633882
-## v⟶p-p⟶p -0.10479358 -0.2288089 0.01922176 0.1304058
-## v⟶v-p⟶p  0.39055360  0.2625286 0.51857858 0.0000000
-## v⟶p-p⟶v -0.08130330 -0.2053186 0.04271205 0.3291041
-## v⟶v-p⟶v  0.41404389  0.2860189 0.54206887 0.0000000
-## v⟶v-v⟶p  0.49534719  0.3695196 0.62117476 0.0000000
+fin.tuk_res <- TukeyHSD(res.fin.aov, which = "Algorithm:Config") %>%
+  tidy %>%
+  ### cleanup
+  separate(comparison, sep=c(3, 4, 7, 11)
+         , into = c("AlgLeft", "Dump", "ConfigLeft", "AlgRight", "ConfigRight")) %>%
+  mutate(ConfigLeft = gsub("-", "", ConfigLeft),
+         ConfigRight = gsub(":","", ConfigRight),
+         AlgRight = gsub(":|-", "", AlgRight),
+         data = "Financial") %>%
+  select(-Dump) %>%
+  ## remove out-group comparisons between different variants, e.g., V1 - V10
+  filter(ConfigLeft == ConfigRight) %>%
+  ## sort by p-value
+  arrange(adj.p.value)
 
 ### Check leverage of the observations for outliers
 res.fin.outliers <- plot(res.fin.aov, 1)
@@ -127,30 +127,28 @@ res.fin.shaps <- shapiro.test(x = aov.fin.resids)
 
 ############################# Auto ANOVA ##############################
 ### Do a two-way anova looking at both algorithm and config and their interaction
-res.auto.aov <- aov(TimeCalc ~ Algorithm * Config, data = autoSingData %>% filter(Config == "V3" || Config == "V4"))
+res.auto.aov <- aov(TimeCalc ~ Algorithm * Config, data = autoSingData)
 
 ### Check the summary to see what is significant, all of it is as expected
 res.auto.sig <- summary(res.auto.aov)
 
 ### Autoally, perform the pair-wise Tukey comparison to test the difference
 ### between groups
-auto.tuk_res <- TukeyHSD(res.auto.aov, which = "Algorithm")
+auto.tuk_res <- TukeyHSD(res.auto.aov, which = "Algorithm:Config") %>%
+  tidy %>%
+### cleanup
+  separate(comparison, sep=c(3, 4, 7, 11)
+         , into = c("AlgLeft", "Dump", "ConfigLeft", "AlgRight", "ConfigRight")) %>%
+  mutate(ConfigLeft = gsub("-", "", ConfigLeft),
+         ConfigRight = gsub(":","", ConfigRight),
+         AlgRight = gsub(":|-", "", AlgRight),
+         data = "Auto") %>%
+  select(-Dump) %>%
+  ## remove out-group comparisons between different variants, e.g., V1 - V10
+  filter(ConfigLeft == ConfigRight) %>%
+  ## sort by p-value
+  arrange(adj.p.value)
 
-## Note that only v-->p and p-->p is not significant
-
-## Tukey multiple comparisons of means
-## 95% family-wise confidence level
-
-## Fit: aov(formula = TimeCalc ~ Algorithm * Config, data = autoSingData)
-
-## $Algorithm
-##                diff        lwr        upr     p adj
-## p⟶v-p⟶p -0.02349029 -0.1497346 0.10275401 0.9633882
-## v⟶p-p⟶p -0.10479358 -0.2288089 0.01922176 0.1304058
-## v⟶v-p⟶p  0.39055360  0.2625286 0.51857858 0.0000000
-## v⟶p-p⟶v -0.08130330 -0.2053186 0.04271205 0.3291041
-## v⟶v-p⟶v  0.41404389  0.2860189 0.54206887 0.0000000
-## v⟶v-v⟶p  0.49534719  0.3695196 0.62117476 0.0000000
 
 ### Check leverage of the observations for outliers
 res.auto.outliers <- plot(res.auto.aov, 1)
@@ -163,3 +161,13 @@ aov.auto.resids <- residuals(object=res.auto.aov)
 
 ## Shapiro-Wilk normality test
 res.auto.shaps <- shapiro.test(x = aov.auto.resids)
+
+
+########################## Plot p-values ##################
+options(scipen = 999)
+rq3pvDF <- rbind(auto.tuk_res, fin.tuk_res) %>% arrange(adj.p.value)
+
+rq3pv <- ggplot(rq3pvDF, aes(x=AlgLeft, y=AlgRight, shape=data)) +
+  geom_point(aes(size=sizeVal)) +
+  geom_jitter() +
+  theme_classic()
