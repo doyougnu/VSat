34fe6140ed6395c22c610ee63c38eb57d9b4c8d1
doyougnu <jmy6342@gmail.com>
2020-05-11 23:05:48 +0200

    tweaking

--- haskell/julia/timingMunger.jl
+++ haskell/julia/timingMunger.jl
@@ -4,7 +4,7 @@ using Query  # Tidyverse
 using Plots  # for plotting
 
 ### get the data in a data frame
-dataFile = "../fin_rq3.csv"
+dataFile = "../auto_timings.csv"
 df = CSV.File(dataFile) |> DataFrame
 
 ### helper functions
@@ -63,4 +63,4 @@ end
 data = mungeDF!(df)
 
 ## write the file
-data |> CSV.write("../data/fin_rq3_singleton.csv")
+data |> CSV.write("../data/auto_timings_1.csv")
