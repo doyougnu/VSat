055be006414652d478f02010b02ed7e91d13c9b6
doyougnu <jmy6342@gmail.com>
2021-02-14 20:39:58 -0800

    cleanup

---
+++ haskell/statisticsScripts/rq12_tests.R
@@ -0,0 +1,121 @@
+library(ggplot2)
+library(dplyr)
+library(cowplot)
+library(tidyr)
+library(latex2exp)
+library(Hmisc)
+library(broom)
+library(ggpubr)
+library(scales)
+library(rstatix)
+
+finRawFile  <- "../munged_data/financial_raw.csv"
+autoRawFile <- "../munged_data/auto_raw.csv"
+
+################# Singleton Analysis ##############################
+finRawDF <- read.csv(file=finRawFile) %>%
+  mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config)) %>%
+  mutate(Algorithm = gsub("-->", "\U27f6", Algorithm), data = "Financial") %>%
+  group_by(Algorithm, DataSet, Config) %>%
+  mutate(TimeCalc = Time - lag(Time, default = 0))
+
+autoRawDF <- read.csv(file=autoRawFile) %>%
+  mutate(Algorithm = as.factor(Algorithm), Config = as.factor(Config)) %>%
+  mutate(Algorithm = gsub("-->", "\U27f6", Algorithm), data = "Auto") %>%
+  group_by(Algorithm, DataSet, Config) %>%
+  mutate(TimeCalc = Time - lag(Time, default = 0))
+
+##################### Financial #############################
+## Algorithms are significant
+fin.alg.res <- kruskal.test(TimeCalc ~ Algorithm, finRawDF)
+
+## Versions are significant as expected
+fin.vers.res <- kruskal.test(TimeCalc ~ Config, finRawDF)
+
+## Solvers are actually not significant by themselves
+fin.slvr.res <- kruskal.test(TimeCalc ~ DataSet, finRawDF)
+
+## Interaction bewtween algorithm and version significant as expected
+fin.alg.conf.inters <- interaction(finRawDF$Algorithm, finRawDF$Config)
+fin.alg.slvr.inters <- interaction(finRawDF$DataSet, finRawDF$Algorithm, finRawDF$Config)
+fin.alg.conf.res    <- kruskal.test(TimeCalc ~ fin.alg.conf.inters, finRawDF)
+fin.alg.slvr.res    <- kruskal.test(TimeCalc ~ fin.alg.slvr.inters, finRawDF)
+
+## Find the pairs which are significant
+fin.pairs <- pairwise.wilcox.test(finRawDF$TimeCalc, fin.alg.conf.inters,
+                                  p.adj="bonf", exact=FALSE,
+                                  paired=FALSE) %>%
+  tidy %>%
+  separate(group1, sep=c(3,4), into = c("AlgLeft", "Dump", "ConfigLeft")) %>%
+  separate(group2, sep=c(3,4), into = c("AlgRight", "Dump2", "ConfigRight")) %>%
+  select(-Dump, -Dump2) %>%
+  filter(ConfigRight == ConfigLeft) %>%
+  mutate(data = "Financial") %>%
+  arrange(p.value)
+
+## We notice here that the p-values are all 1 after the bonferroni adjustment.
+## Solver is not statistically significant
+fin.slvr.pairs <- pairwise.wilcox.test(finRawDF$TimeCalc, fin.alg.slvr.inters,
+                                  p.adj="bonf", exact=TRUE,
+                                  paired=FALSE) %>%
+  tidy %>%
+  separate(group1, sep="\\.", into = c("SolverLeft", "AlgLeft", "ConfigLeft")) %>%
+  separate(group2, sep="\\.", into = c("SolverRight", "AlgRight", "ConfigRight")) %>%
+  filter(ConfigRight == ConfigLeft) %>%
+  arrange(p.value)
+
+##################### Auto #############################
+## Algorithms are significant
+auto.alg.res <- kruskal.test(TimeCalc ~ Algorithm, autoRawDF)
+
+## Versions are significant as expected
+auto.vers.res <- kruskal.test(TimeCalc ~ Config, autoRawDF)
+
+## Solvers are actually not significant by themselves
+auto.slvr.res <- kruskal.test(TimeCalc ~ DataSet, autoRawDF)
+
+## Interaction bewtween algorithm and version significant as expected
+auto.alg.conf.inters <- interaction(autoRawDF$Algorithm, autoRawDF$Config)
+auto.alg.slvr.inters <- interaction(autoRawDF$DataSet, autoRawDF$Algorithm, autoRawDF$Config)
+auto.alg.conf.res    <- kruskal.test(TimeCalc ~ auto.alg.conf.inters, autoRawDF)
+auto.alg.slvr.res    <- kruskal.test(TimeCalc ~ auto.alg.slvr.inters, autoRawDF)
+
+## Autod the pairs which are significant
+auto.pairs <- pairwise.wilcox.test(autoRawDF$TimeCalc, auto.alg.conf.inters,
+                                  p.adj="bonf", exact=FALSE,
+                                  paired=FALSE) %>%
+  tidy %>%
+  separate(group1, sep=c(3,4), into = c("AlgLeft", "Dump", "ConfigLeft")) %>%
+  separate(group2, sep=c(3,4), into = c("AlgRight", "Dump2", "ConfigRight")) %>%
+  select(-Dump, -Dump2) %>%
+  filter(ConfigRight == ConfigLeft) %>%
+  mutate(data = "Auto") %>%
+  arrange(p.value)
+
+## We notice here that the p-values are all 1 after the bonferroni adjustment.
+## Solver is not statistically significant for both datasets
+auto.slvr.pairs <- pairwise.wilcox.test(autoRawDF$TimeCalc, auto.alg.slvr.inters,
+                                  p.adj="bonf", exact=TRUE,
+                                  paired=FALSE) %>%
+  tidy %>%
+  separate(group1, sep="\\.", into = c("SolverLeft", "AlgLeft", "ConfigLeft")) %>%
+  separate(group2, sep="\\.", into = c("SolverRight", "AlgRight", "ConfigRight")) %>%
+  filter(ConfigRight == ConfigLeft) %>%
+  arrange(p.value)
+
+## ########################## Combined data frame ##################
+options(scipen = 999)
+rq3pvDF <- rbind(auto.pairs, fin.pairs) %>%
+  arrange(p.value) %>%
+  mutate(Significance = case_when(p.value <= 0.05 ~ "Significant",
+                                  TRUE ~ "Not Significant"),
+         Version = factor(ConfigLeft, levels =
+                                        c("V1", "V2", "V3", "V4", "V5", "V6",
+                                          "V7", "V8", "V9", "V10")))
+
+### It seems strange given rq3 plot that only v1 is significant for auto by
+### algorithms, given that the averages between groups are so different. This is
+### because there is a high degree of variance between the samples, you can
+### observe this here:
+
+## ggplot(autoRawDF, aes( x = Config, y = TimeCalc, color=Algorithm)) + geom_boxplot() + geom_jitter()
--- haskell/statisticsScripts/rq4_plots.R
+++ haskell/statisticsScripts/rq4_plots.R
@@ -21,13 +21,13 @@ data <- rbind(finData, autoData)
 
 ################# Singleton  Plots ##############################
 ## Need to filter out version variants which have choices for rq1
-rq3DF <- data %>% filter(ChcCount == 0) %>%
+rq4DF <- data %>% filter(ChcCount == 0) %>%
   mutate(plotOrdering = as.numeric(substring(Config, 2))) %>%
   mutate(Config = factor(Config, levels = c("V1", "V2", "V3", "V4", "V5", "V6",
                                             "V7", "V8", "V9", "V10")))
 
 ## ## custom breaks for the facets
-breaksRq3 <- function(x) {
+breaksRq4 <- function(x) {
   if (max(x) < 4) {
     ## then we are in fin, NA to just have R find the max
     seq(0, 1.2, 0.10)
@@ -36,31 +36,32 @@ breaksRq3 <- function(x) {
   }
 }
 
-rq3 <- function(df) {
+rq4 <- function(df,title) {
   ggplot(df, aes(x=Config, y=Mean, fill=Algorithm, shape=Algorithm, color=Algorithm)) +
     geom_point(size=6) +
     scale_shape_manual(values = c(1,6,5,17)) +
     theme_classic() +
-    scale_y_continuous(limits=c(0, NA), breaks=breaksRq3) +
+    scale_y_continuous(limits=c(0, NA), breaks=breaksRq4) +
     facet_wrap(DataSet ~ .) +
     ## stat_summary(fun.data="mean_sdl"
     ##            , fun.args = list(mult=2)
     ##            , geom="pointrange"
     ##            , color="black"
     ##            , size=0.65) +
-    ggtitle("RQ3: Overhead of Variational Solving on Plain Formulas") +
+    ggtitle(title) +
     ylab("Time [s] to solve single version variant") +
     xlab("Feature Model Version") +
-    theme(legend.position = c(0.42,0.75),
+    theme(legend.position = c(0.42,0.85),
           legend.key.size = unit(.65,'cm')) +
     theme(panel.grid.major.y = element_line(color = "grey")) +
     coord_flip()
 }
 
-rq3_auto <- rq3(rq3DF %>% filter(data == "Auto"))
-rq3_fin  <- rq3(rq3DF %>% filter(data == "Fin"))
+rq4_auto <- rq4(rq4DF %>% filter(data == "Auto"), "(Auto) RQ4: Overhead of Variational Solving on Plain Formulas")
+rq4_fin  <- rq4(rq4DF %>% filter(data == "Fin"), "(Financial) RQ4: Overhead of Variational Solving on Plain Formulas")
 
-## ## ggsave("../plots/RQ3.png", plot = rq3, height = 4, width = 7, device = "png")
+ggsave("../plots/RQ4_Auto.png", plot = rq4_auto, height = 6, width = 7, device = "png")
+ggsave("../plots/RQ4_Fin.png", plot = rq4_fin, height = 6, width = 7, device = "png")
 
 
-slow_down <- rq3DF %>% group_by(data,DataSet,Algorithm) %>%  summarise(AvgMean = mean(Mean))
+slow_down <- rq4DF %>% group_by(data,DataSet,Algorithm) %>%  summarise(AvgMean = mean(Mean))
